c5e3d4eb568051e694fd3aaaba8fa88092e14b90
Lorenzo <lorenzo.padrini@epfl.ch>
2024-03-19 13:02:36 +0100

    feat: add ktfmt to project

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,5 @@
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.ncorti.ktfmt.gradle") version "0.16.0"
 }
