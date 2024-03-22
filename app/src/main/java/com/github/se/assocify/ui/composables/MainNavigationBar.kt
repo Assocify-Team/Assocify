e94586324f20e1d771a82a1f61c00e402d017314
SidonieBouthors <sidonie.bouthors@epfl.ch>
2024-03-22 19:11:01 +0100

    feat: create MainNavigationBar component

---
+++ app/src/main/java/com/github/se/assocify/ui/composables/MainNavigationBar.kt
@@ -0,0 +1,28 @@
+package com.github.se.assocify.ui.composables
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import com.github.se.assocify.Screen
+
+@Composable
+fun MainNavigationBar(
+    tabList: List<Screen>,
+    selectedTab: Screen,
+) {
+    NavigationBar() {
+        tabList.forEach { tab ->
+            NavigationBarItem(
+                selected = tab == selectedTab,
+                onClick = { /*TODO*/ },
+                label = { Text(stringResource(id = tab.labelId)) },
+                icon = { if (tab.iconId != null) { Icon(painterResource(tab.iconId), contentDescription = null) } },
+            )
+        }
+    }
+}
+
