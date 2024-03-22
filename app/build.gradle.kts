a4603942541f997976fa85c2caf22d21ce4ed42a
Lorenzo <lorenzo.padrini@epfl.ch>
2024-03-22 17:31:53 +0100

    fix: added properties of sonarcloud so that it works with JacocoTestReport

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -155,5 +155,11 @@ sonar {
         property("sonar.projectKey", "Assocify-Team_Assocify")
         property("sonar.organization", "assocify-team")
         property("sonar.host.url", "https://sonarcloud.io")
+        // Comma-separated paths to the various directories containing the *.xml JUnit report files. Each path may be absolute or relative to the project base directory.
+        property("sonar.junit.reportPaths", "${project.layout.buildDirectory.get()}/test-results/testDebugunitTest/")
+        // Paths to xml files with Android Lint issues. If the main flavor is changed, this file will have to be changed too.
+        property("sonar.androidLint.reportPaths", "${project.layout.buildDirectory.get()}/reports/lint-results-debug.xml")
+        // Paths to JaCoCo XML coverage report files.
+        property("sonar.coverage.jacoco.xmlReportPaths", "${project.layout.buildDirectory.get()}/reports/jacoco/jacocoTestReport/jacocoTestReport.xml")
     }
 }
