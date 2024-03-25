e8aefb1ad95c069c53ba72785c2d23d134a2c047
Sekoia <sekoia@sekoia.dev>
2024-03-25 19:32:53 +0100

    feat: created basic firebase API
    Co-authored-by: ZiZouet <91062845+zizouet@users.noreply.github.com>

---
+++ app/src/main/java/com/github/se/assocify/model/database/FirebaseAPI.kt
@@ -0,0 +1,16 @@
+package com.github.se.assocify.model.database
+
+import com.google.firebase.firestore.FirebaseFirestore
+
+abstract class FirebaseApi(val db: FirebaseFirestore) {
+    // Define common properties
+    abstract val collectionName: String
+
+
+    fun getNewId() = db.collection(collectionName).document().id
+
+    fun update(id: String, data: Map<String, Any>) =
+        db.collection(collectionName).document(id).update(data)
+
+    fun delete(id: String) = db.collection(collectionName).document(id).delete()
+}
