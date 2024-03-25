23e82e34543674112550c7ca180af5bfa019f48f
Sekoia <sekoia@sekoia.dev>
2024-03-25 20:14:33 +0100

    style: reformatted FirebaseAPI.kt

--- app/src/main/java/com/github/se/assocify/model/database/FirebaseAPI.kt
+++ app/src/main/java/com/github/se/assocify/model/database/FirebaseAPI.kt
@@ -3,14 +3,13 @@ package com.github.se.assocify.model.database
 import com.google.firebase.firestore.FirebaseFirestore
 
 abstract class FirebaseApi(val db: FirebaseFirestore) {
-    // Define common properties
-    abstract val collectionName: String
+  // Define common properties
+  abstract val collectionName: String
 
+  fun getNewId() = db.collection(collectionName).document().id
 
-    fun getNewId() = db.collection(collectionName).document().id
+  fun update(id: String, data: Map<String, Any>) =
+      db.collection(collectionName).document(id).update(data)
 
-    fun update(id: String, data: Map<String, Any>) =
-        db.collection(collectionName).document(id).update(data)
-
-    fun delete(id: String) = db.collection(collectionName).document(id).delete()
+  fun delete(id: String) = db.collection(collectionName).document(id).delete()
 }
