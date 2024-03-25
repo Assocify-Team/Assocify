b67e6e3dde88513f3ab145f44f3fc2d4e79294be
SidonieBouthors <sidonie.bouthors@epfl.ch>
2024-03-25 19:49:33 +0100

    test: update main nav bar test

--- app/src/androidTest/java/com/github/se/assocify/composables/MainNavigationBarTest.kt
+++ app/src/androidTest/java/com/github/se/assocify/composables/MainNavigationBarTest.kt
@@ -1,27 +1,19 @@
 package com.github.se.assocify.composables
 
-import androidx.compose.ui.test.SemanticsNodeInteractionsProvider
-import androidx.compose.ui.test.assert
-import androidx.compose.ui.test.assertAll
 import androidx.compose.ui.test.assertCountEquals
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onAllNodesWithTag
-import androidx.compose.ui.test.onFirst
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
-import com.github.se.assocify.MAIN_TABS_LIST
-import com.github.se.assocify.Screen
+import com.github.se.assocify.navigation.MAIN_TABS_LIST
+import com.github.se.assocify.navigation.Destination
 import com.github.se.assocify.ui.composables.MainNavigationBar
 import com.kaspersky.components.composesupport.config.withComposeSupport
 import org.junit.Test
 import org.junit.runner.RunWith
 import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
-import io.github.kakaocup.compose.node.element.ComposeScreen
-import io.github.kakaocup.compose.node.element.KNode
-import junit.framework.TestCase.assertEquals
 import org.junit.Before
 import org.junit.Rule
 
@@ -33,21 +25,15 @@ class MainNavigationBarTest: TestCase(kaspressoBuilder = Kaspresso.Builder.withC
     fun testSetup() {
         composeTestRule.setContent { MainNavigationBar(
             tabList = MAIN_TABS_LIST,
-            selectedTab = Screen.Home
+            selectedTab = Destination.Home
         ) }
     }
     @Test
     fun iconsDisplayed() {
-        composeTestRule.onNodeWithTag("mainNavBar").assertIsDisplayed()
-        composeTestRule.onAllNodesWithTag("mainNavBarItem")
-            .assertCountEquals(5)
+        with (composeTestRule) {
+            onNodeWithTag("mainNavBar").assertIsDisplayed()
+            onAllNodesWithTag("mainNavBarItem")
+                .assertCountEquals(5)
+        }
     }
 }
-
-/*class MainNavigationBarSemantics(semanticsProvider: SemanticsNodeInteractionsProvider) :
-    ComposeScreen<MainNavigationBarSemantics>(
-        semanticsProvider = semanticsProvider,
-        viewBuilderAction = { hasTestTag("mainNavBar") }
-    ) {
-
-    }*/
