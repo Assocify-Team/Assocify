6e6c2c9c45c9e525fab936790b22fef6190dd330
sarahbadr17 <sarahbadr@live.com>
2024-03-25 20:58:01 +0100

    feat: adding tests

---
+++ app/src/androidTest/java/com/github/se/assocify/SelectAssociationTest.kt
@@ -0,0 +1,9 @@
+package com.github.se.assocify
+
+import androidx.compose.ui.test.hasScrollAction
+import io.github.kakaocup.compose.node.element.ComposeScreen.Companion.onComposeScreen
+import org.junit.Test
+
+class SelectAssociationTest {
+
+}
