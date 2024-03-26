b5f93f3d2d0ba69172daea4c8c25d4c22f364f7b
polymeth <rayan.boucheny@epfl.ch>
2024-03-26 16:36:32 +0100

    feat: added receipt items

--- app/src/main/java/com/github/se/assocify/ui/screens/Treasury/TreasuryScreen.kt
+++ app/src/main/java/com/github/se/assocify/ui/screens/Treasury/TreasuryScreen.kt
@@ -7,22 +7,29 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Tab
@@ -40,8 +47,16 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.github.se.assocify.R
 import kotlinx.coroutines.launch
 import com.github.se.assocify.ui.theme.AssocifyTheme
 
@@ -107,7 +122,7 @@ fun TreasuryMainScreen() {
                         modifier = Modifier
                             .fillMaxSize()
                             .tabIndicatorOffset(tabPositions[pagerState.currentPage])
-                            .size(width = 1.dp, height = 4.dp)
+                            .size(width = 10.dp, height = 3.dp)
                             .background(color = MaterialTheme.colorScheme.primary, shape = RoundedCornerShape(8.dp))
                     )
                 }
@@ -124,7 +139,7 @@ fun TreasuryMainScreen() {
                             color = if (pagerState.currentPage == PAGE_RECEIPT_INDEX) {
                                 MaterialTheme.colorScheme.primary
                             } else {
-                                MaterialTheme.colorScheme.primary.copy(alpha = 0.6f)
+                                MaterialTheme.colorScheme.primary.copy(alpha = 0.7f)
                             }
                         )
                     }
@@ -141,7 +156,7 @@ fun TreasuryMainScreen() {
                             color = if (pagerState.currentPage == PAGE_BUDGET_INDEX) {
                                 MaterialTheme.colorScheme.primary
                             } else {
-                                MaterialTheme.colorScheme.primary.copy(alpha = 0.8f)
+                                MaterialTheme.colorScheme.primary.copy(alpha = 0.7f)
                             }
                         )
                     }
@@ -158,7 +173,7 @@ fun TreasuryMainScreen() {
                             color = if (pagerState.currentPage == PAGE_BALANCE_INDEX) {
                                 MaterialTheme.colorScheme.primary
                             } else {
-                                MaterialTheme.colorScheme.primary.copy(alpha = 0.8f)
+                                MaterialTheme.colorScheme.primary.copy(alpha = 0.7f)
                             }
                         )
                     }
@@ -184,9 +199,24 @@ fun TreasuryMainScreen() {
     }
 }
 
+// ------------- PAGES ------------- //
+
 @Composable
 private fun MyReceiptPage() {
-
+    //val receiptNames = listOf("Grocery Shopping", "Restaurant", "Gas Station", "Coffee Shop")
+    val receiptNames = List(50) { "Receipt $it" }
+    LazyColumn(
+        modifier = Modifier.testTag("ReceiptList"),
+        verticalArrangement = Arrangement.spacedBy(0.dp, Alignment.Top),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        receiptNames.forEach { receiptName ->
+            item {
+                ReceiptItem(receiptName)
+                Divider(modifier = Modifier.padding(start = 20.dp, end = 20.dp))
+            }
+        }
+    }
 }
 
 @Composable
@@ -199,8 +229,79 @@ private fun BalancePage() {
 
 }
 
+// ------------- ELEMENTS ------------- //
+
+@Composable
+private fun ReceiptItem(receiptName: String) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(6.dp)
+            .height(70.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(start = 20.dp)
+        ) {
+            Text(
+                text = "17/04/2002",
+                modifier = Modifier.padding(top = 6.dp),
+                style = TextStyle(
+                    fontSize = 12.sp,
+                    lineHeight = 16.sp,
+                    color = Color(0xFF505050),
+                    letterSpacing = 0.5.sp,
+                )
+            )
+            Text(
+                text = receiptName,
+                style = TextStyle(
+                    fontSize = 16.sp,
+                    lineHeight = 24.sp,
+                    color = Color(0xFF000000),
+                    letterSpacing = 0.sp,
+                )
+            )
+            Text(
+                text = "Super description mdr",
+                style = TextStyle(
+                    fontSize = 14.sp,
+                    lineHeight = 24.sp,
+                    color = Color(0xFF505050),
+                    letterSpacing = 0.sp,
+                )
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .padding(end = 16.dp, top = 8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "80.-",
+                style = TextStyle(
+                    fontSize = 14.sp,
+                    lineHeight = 24.sp,
+                    color = Color(0xFF505050),
+                    letterSpacing = 0.sp,
+                )
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Icon(
+                modifier = Modifier.size(20.dp),
+                imageVector = Icons.Filled.ShoppingCart,
+                contentDescription = "Arrow icon",
+            )
+        }
+
+    }
+
+}
+
 @Preview
 @Composable
 private fun PreviewCardsScreen() {
     TreasuryMainScreen()
 }
+
