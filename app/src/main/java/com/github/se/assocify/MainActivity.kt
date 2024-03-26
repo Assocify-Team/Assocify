18b3ab86e6cac538054f48e8f725109c414075f4
sarahbadr17 <sarahbadr@live.com>
2024-03-26 16:12:21 +0100

    fix: change mainactivity

--- app/src/main/java/com/github/se/assocify/MainActivity.kt
+++ app/src/main/java/com/github/se/assocify/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.github.se.assocify.ui.screens.SelectAssociation
 import com.github.se.assocify.ui.theme.AssocifyTheme
 
 class MainActivity : ComponentActivity() {
