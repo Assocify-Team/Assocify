97c6492a98e0b26155c229848d6a2eca51de3b91
ZiZouet <91062845+zizouet@users.noreply.github.com>
2024-03-29 17:08:29 +0100

    feat: add permissions

---
+++ app/src/main/java/com/github/se/assocify/model/entities/Permission.kt
@@ -0,0 +1,11 @@
+package com.github.se.assocify.model.entities
+
+enum class Permission {
+  CREATE_ROLE,
+  ASSIGN_ROLE,
+  EDIT_PERMISSIONS,
+  CREATE_ASSOCIATION,
+  DELETE_ASSOCIATION,
+  CREATE_EVENT,
+  DELETE_EVENT
+}
