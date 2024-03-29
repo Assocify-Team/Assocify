e6cb56e43070df6d5994b4b3c587a196e0fadd16
ZiZouet <91062845+zizouet@users.noreply.github.com>
2024-03-29 17:10:00 +0100

    feat: user integration with roles and permissions

--- app/src/main/java/com/github/se/assocify/model/entities/User.kt
+++ app/src/main/java/com/github/se/assocify/model/entities/User.kt
@@ -1,5 +1,34 @@
 package com.github.se.assocify.model.entities
 
-data class User(val uid: String, val name: String, val role: Role) {
-  constructor() : this("", "", Role(""))
+class User(val uid: String,
+                val name: String,
+                role: Role = Role.USER) {
+
+  private var _role: Role
+
+
+  /** Default constructor used for Firebase deserialization */
+  constructor() : this("", "", Role.USER)
+
+  init {
+    _role = role
+  }
+  val role: Role
+    get() = _role
+
+
+  /**
+   * Grants a new role to the user
+   *
+   * @param grantedBy the user granting the role
+   * @param newRole the new role to grant
+   */
+  fun grantRole(grantedBy: User, newRole: Role) {
+    if (grantedBy.role.canGrantRoleTo(this,newRole)) {
+      _role = newRole
+    } else {
+      throw Exception("User does not have permission to grant role")
+    }
+  }
+
 }
