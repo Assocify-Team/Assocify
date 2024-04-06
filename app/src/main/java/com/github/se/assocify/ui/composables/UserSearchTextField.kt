6f6faa3b73fb4cb67d4de2a920b25ca5f10d17b3
SidonieBouthors <sidonie.bouthors@epfl.ch>
2024-04-06 20:57:56 +0200

    feat: user search text field

---
+++ app/src/main/java/com/github/se/assocify/ui/composables/UserSearchTextField.kt
@@ -0,0 +1,16 @@
+package com.github.se.assocify.ui.composables
+
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.github.se.assocify.model.entities.User
+
+@Composable
+fun UserSearchTextField(
+    value: String,
+    onUserSelect: (User?) -> Unit,
+    modifier: Modifier = Modifier,
+    label: @Composable (() -> Unit)? = null
+) {
+  OutlinedTextField(value = value, onValueChange = {}, modifier = modifier, label = label)
+}
