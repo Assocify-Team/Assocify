eb941344e965b16c6a84a174a7ee1cbb373cc794
SidonieBouthors <sidonie.bouthors@epfl.ch>
2024-04-08 00:09:20 +0200

    feat: add utils for date and price

---
+++ app/src/main/java/com/github/se/assocify/ui/util/DateUtil.kt
@@ -0,0 +1,21 @@
+package com.github.se.assocify.ui.util
+
+import java.time.format.DateTimeFormatter
+
+object DateUtil {
+
+    const val NULL_DATE_STRING = "--/--/--"
+    fun toString(date: java.time.LocalDate?): String {
+        if (date == null) {
+            return NULL_DATE_STRING
+        }
+        return date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
+    }
+
+    fun toDate(date: String): java.time.LocalDate? {
+        if (date.isEmpty() || date == NULL_DATE_STRING) {
+            return null
+        }
+        return java.time.LocalDate.parse(date, DateTimeFormatter.ofPattern("dd/MM/yyyy"))
+    }
+}
