e0ea8a7220353940b0c2eee41860f736136c9446
SidonieBouthors <sidonie.bouthors@epfl.ch>
2024-04-09 18:00:27 +0200

    style: ktfmt format

--- app/src/main/java/com/github/se/assocify/ui/util/DateUtil.kt
+++ app/src/main/java/com/github/se/assocify/ui/util/DateUtil.kt
@@ -4,18 +4,19 @@ import java.time.format.DateTimeFormatter
 
 object DateUtil {
 
-    const val NULL_DATE_STRING = "--/--/--"
-    fun toString(date: java.time.LocalDate?): String {
-        if (date == null) {
-            return NULL_DATE_STRING
-        }
-        return date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
-    }
+  const val NULL_DATE_STRING = "--/--/--"
 
-    fun toDate(date: String): java.time.LocalDate? {
-        if (date.isEmpty() || date == NULL_DATE_STRING) {
-            return null
-        }
-        return java.time.LocalDate.parse(date, DateTimeFormatter.ofPattern("dd/MM/yyyy"))
+  fun toString(date: java.time.LocalDate?): String {
+    if (date == null) {
+      return NULL_DATE_STRING
     }
+    return date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
+  }
+
+  fun toDate(date: String): java.time.LocalDate? {
+    if (date.isEmpty() || date == NULL_DATE_STRING) {
+      return null
+    }
+    return java.time.LocalDate.parse(date, DateTimeFormatter.ofPattern("dd/MM/yyyy"))
+  }
 }
--- app/src/main/java/com/github/se/assocify/ui/util/PriceUtil.kt
+++ app/src/main/java/com/github/se/assocify/ui/util/PriceUtil.kt
@@ -2,14 +2,15 @@ package com.github.se.assocify.ui.util
 
 object PriceUtil {
 
-    const val ZERO_PRICE_STRING = "0.0"
-    fun toString(price: Double): String {
-        /*TODO: Implement currency formatting and add appropriate checks*/
-        return price.toString()
-    }
+  const val ZERO_PRICE_STRING = "0.0"
 
-    fun toDouble(price: String): Double {
-        /*TODO: Remove - and add appropriate checks*/
-        return price.toDouble()
-    }
+  fun toString(price: Double): String {
+    /*TODO: Implement currency formatting and add appropriate checks*/
+    return price.toString()
+  }
+
+  fun toDouble(price: String): Double {
+    /*TODO: Remove - and add appropriate checks*/
+    return price.toDouble()
+  }
 }
