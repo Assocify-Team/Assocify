e1b3a21e9045411c4c34b0107292f7ed7c83a53c
SidonieBouthors <sidonie.bouthors@epfl.ch>
2024-04-10 18:13:03 +0200

    build: remove duplicates and add icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,11 +90,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview:1.6.4")
     implementation("androidx.compose.material3:material3:1.2.1")
     implementation("androidx.navigation:navigation-compose:2.7.7")
-
-    implementation("androidx.compose.ui:ui:1.6.4")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.6.4")
-    implementation("androidx.compose.material3:material3:1.2.1")
-    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.compose.material:material-icons-extended")
 
     implementation("com.google.maps.android:maps-compose:4.3.0")
     implementation("com.google.maps.android:maps-compose-utils:4.3.0")
