f8fb53a26c02ce89100f876bd11d06dd4a0c361a
Mai-LinhC <mai-linh.cordonnier@epfl.ch>
2024-04-10 18:55:16 +0200

    feat: committing not to lose data, not working because of dialog

--- app/src/main/java/com/github/se/assocify/ui/screens/createAsso/CreateAssoScreen.kt
+++ app/src/main/java/com/github/se/assocify/ui/screens/createAsso/CreateAssoScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -29,25 +30,33 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import com.github.se.assocify.R
+import com.github.se.assocify.model.entities.Role
+import com.github.se.assocify.model.entities.User
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CreateAssoScreen(viewmodel: CreateAssoViewmodel = CreateAssoViewmodel()) {
 
   val state by viewmodel.uiState.collectAsState()
+    val editMember by remember { mutableStateOf(User("", "", Role(""))) }
 
   Scaffold(
       modifier = Modifier.testTag("createAssoScreen"),
       topBar = {
         TopAppBar(
-            modifier = Modifier.fillMaxWidth().testTag("TopAppBar"),
+            modifier = Modifier
+                .fillMaxWidth()
+                .testTag("TopAppBar"),
             navigationIcon = {
               IconButton(onClick = { /* TODO : go back to previous screen */}) {
                 Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
@@ -57,7 +66,9 @@ fun CreateAssoScreen(viewmodel: CreateAssoViewmodel = CreateAssoViewmodel()) {
       },
       contentWindowInsets = WindowInsets(20.dp, 10.dp, 20.dp, 20.dp)) { innerPadding ->
         Column(
-            modifier = Modifier.fillMaxSize().padding(innerPadding),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
             verticalArrangement = Arrangement.spacedBy(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
@@ -66,7 +77,9 @@ fun CreateAssoScreen(viewmodel: CreateAssoViewmodel = CreateAssoViewmodel()) {
               verticalAlignment = Alignment.CenterVertically,
               modifier = Modifier.fillMaxWidth()) {
                 OutlinedIconButton(
-                    modifier = Modifier.padding(top = 8.dp).testTag("logo"),
+                    modifier = Modifier
+                        .padding(top = 8.dp)
+                        .testTag("logo"),
                     onClick = {
                       /* TODO : can add association logo // note : nowhere to put it yet because picture not handled in DB */
                     }) {
@@ -78,18 +91,25 @@ fun CreateAssoScreen(viewmodel: CreateAssoViewmodel = CreateAssoViewmodel()) {
                     value = state.name,
                     onValueChange = { viewmodel.setName(it) },
                     label = { Text("Association Name") },
-                    modifier = Modifier.fillMaxWidth().testTag("name"))
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .testTag("name"))
               }
 
           LazyColumn(
-              modifier = Modifier.fillMaxWidth().weight(1f).testTag("MemberList"),
+              modifier = Modifier
+                  .fillMaxWidth()
+                  .weight(1f)
+                  .testTag("MemberList"),
               verticalArrangement = Arrangement.spacedBy(0.dp, Alignment.Top),
               horizontalAlignment = Alignment.CenterHorizontally) {
                 state.members.forEach { member ->
                   item {
                     ListItem(
                         modifier =
-                            Modifier.clip(RoundedCornerShape(10.dp)).testTag("MemberListItem"),
+                        Modifier
+                            .clip(RoundedCornerShape(10.dp))
+                            .testTag("MemberListItem"),
                         headlineContent = { Text(member.name) },
                         overlineContent = { Text(member.role.name) },
                         leadingContent = {
@@ -112,17 +132,41 @@ fun CreateAssoScreen(viewmodel: CreateAssoViewmodel = CreateAssoViewmodel()) {
               horizontalAlignment = Alignment.CenterHorizontally,
               modifier = Modifier.fillMaxWidth()) {
                 OutlinedButton(
-                    onClick = { /* TODO : add members to list : need to open listDialog... call other function?? */},
-                    modifier = Modifier.fillMaxWidth().testTag("addMember")) {
+                    onClick = { /* TODO : add members to list : need to open listDialog... call other function?? */
+                              editMember = User("uid", "name", Role("role"))},
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .testTag("addMember")) {
                       Icon(Icons.Default.Add, contentDescription = "Add members")
                       Text("Add members")
                     }
                 Button( // TODO : should be disabled if no member/name empty ?
                     onClick = { viewmodel.saveAsso() },
-                    modifier = Modifier.fillMaxWidth().testTag("create")) {
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .testTag("create")) {
                       Text("Create")
                     }
               }
         }
+
+      // open dialog to edit member
+      if (editMember.uid != "") {
+          Dialog(onDismissRequest = { /*TODO : should close dialog... idk how to exploit a var here with the function */ }) {
+              ElevatedCard {
+                  // temporary UI to see if dialog opens
+                  Column(
+                      modifier = Modifier
+                          .padding(16.dp)
+                          .fillMaxWidth(),
+                      horizontalAlignment = Alignment.CenterHorizontally) {
+                      Text("Edit member")
+                      // TODO : add fields to edit member
+                  }
+              }
+          }
+      }
+
       }
 }
+
