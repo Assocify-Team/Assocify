04617077ba9461fe2c8d029ed57cdf7f3abe6e6b
Sekoia <sekoia@sekoia.dev>
2024-04-12 01:42:47 +0200

    test: make refactored API tests work

--- app/src/test/java/com/github/se/assocify/model/database/UserAPITest.kt
+++ app/src/test/java/com/github/se/assocify/model/database/UserAPITest.kt
@@ -1,99 +1,120 @@
 package com.github.se.assocify.model.database
-/*
-import androidx.test.ext.junit.runners.AndroidJUnit4
+
 import com.github.se.assocify.model.entities.Role
 import com.github.se.assocify.model.entities.User
-import com.google.android.gms.tasks.Tasks
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.QuerySnapshot
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit4.MockKRule
 import io.mockk.junit5.MockKExtension
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Assert.fail
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.mockito.Mock
-import org.mockito.Mockito
 
 @MockKExtension.ConfirmVerification
 class UserAPITest {
+  @get:Rule val mockkRule = MockKRule(this)
+  @MockK private lateinit var db: FirebaseFirestore
 
-  @Mock private lateinit var db: FirebaseFirestore
+  @MockK private lateinit var documentSnapshot: DocumentSnapshot
+  @MockK private lateinit var documentReference: DocumentReference
+  @MockK private lateinit var collectionReference: CollectionReference
+
+  @MockK private lateinit var query: QuerySnapshot
 
   private lateinit var userAPI: UserAPI
-  private val documentSnapshot = Mockito.mock(DocumentSnapshot::class.java)
-  private val documentReference = Mockito.mock(DocumentReference::class.java)
-  private val collectionReference = Mockito.mock(CollectionReference::class.java)
-  private val user = User("testId", "testName", Role("testId"))
+  private val testUser = User("testId", "testName", Role("testId"))
 
   @Before
   fun setup() {
-    db = Mockito.mock(FirebaseFirestore::class.java)
+    every { documentSnapshot.exists() } returns true
+    every { documentSnapshot.toObject(User::class.java) } returns testUser
+    every { db.collection("users").document("testId") } returns documentReference
+    every { db.collection("users") } returns collectionReference
+    every { collectionReference.document("testId") } returns documentReference
+    every { query.documents } returns listOf(documentSnapshot)
+
     userAPI = UserAPI(db)
   }
 
   @Test
   fun testGetUser() {
+    every { documentReference.get() } returns APITestUtils.mockSuccessfulTask(documentSnapshot)
 
-    Mockito.`when`(documentSnapshot.exists()).thenReturn(true)
-    Mockito.`when`(documentSnapshot.toObject(User::class.java)).thenReturn(user)
-    Mockito.`when`(documentReference.get()).thenReturn(Tasks.forResult(documentSnapshot))
+    val successMock = mockk<(User) -> Unit>(relaxed = true)
+    userAPI.getUser(testUser.uid, successMock, { fail("Should not fail") })
 
-    Tasks.forResult(documentSnapshot)
-    Mockito.`when`(db.collection(Mockito.any())).thenReturn(Mockito.mock())
-    Mockito.`when`(db.collection(Mockito.any()).document(Mockito.any()))
-        .thenReturn(documentReference)
-    var result: User? = null
-    Tasks.await(userAPI.getUser(user.uid) { user: User -> result = user })
+    verify(timeout = 100) { successMock.invoke(testUser) }
 
-    Mockito.verify(db).collection(userAPI.collectionName)
-    Mockito.verify(db.collection(userAPI.collectionName)).document(user.uid)
-    Mockito.verify(db.collection(userAPI.collectionName).document(user.uid)).get()
-    assert(result == user)
+    every { documentReference.get() } returns APITestUtils.mockFailingTask()
+
+    val failureMock = mockk<(Exception) -> Unit>(relaxed = true)
+    userAPI.getUser(testUser.uid, { fail("Should not succeed") }, failureMock)
+
+    verify(timeout = 100) { failureMock.invoke(any()) }
   }
 
   @Test
   fun testGetAllUsers() {
-    Mockito.`when`(documentSnapshot.exists()).thenReturn(true)
-    Mockito.`when`(db.collection(Mockito.any())).thenReturn(collectionReference)
-    val query = Tasks.forResult(Mockito.mock(QuerySnapshot::class.java))
-    Mockito.`when`(db.collection(Mockito.any()).get()).thenReturn(query)
+    every { collectionReference.get() } returns APITestUtils.mockSuccessfulTask(query)
 
-    Mockito.`when`(query.result!!.documents).thenReturn(listOf(documentSnapshot))
-    Mockito.`when`(listOf(documentSnapshot).map { document -> document.toObject(User::class.java) })
-        .thenReturn(listOf(user))
-    Mockito.`when`(documentSnapshot.toObject(User::class.java)).thenReturn(user)
-    var result: List<User>? = null
-    Tasks.await(userAPI.getAllUsers { users: List<User> -> result = users })
-    Mockito.verify(db).collection(userAPI.collectionName)
-    Mockito.verify(db.collection(userAPI.collectionName)).get()
-    assert(result?.size == 1)
-    assert(result?.get(0) == user)
+    val successMock = mockk<(List<User>) -> Unit>(relaxed = true)
+
+    userAPI.getAllUsers(successMock, { fail("Should not fail") })
+
+    verify { successMock.invoke(listOf(testUser)) }
+
+    every { collectionReference.get() } returns APITestUtils.mockFailingTask()
+
+    val failureMock = mockk<(Exception) -> Unit>(relaxed = true)
+
+    userAPI.getAllUsers({ fail("Should not succeed") }, failureMock)
+
+    verify { failureMock.invoke(any()) }
   }
 
   @Test
   fun testAddUser() {
-    Mockito.`when`(db.collection(Mockito.any())).thenReturn(collectionReference)
-    Mockito.`when`(db.collection(Mockito.any()).document(Mockito.any()))
-        .thenReturn(documentReference)
-    Mockito.`when`(documentReference.set(user)).thenReturn(Tasks.forResult(null))
-    Tasks.await(userAPI.addUser(user))
-    Mockito.verify(db).collection(userAPI.collectionName)
-    Mockito.verify(db.collection(userAPI.collectionName)).document(user.uid)
-    Mockito.verify(db.collection(userAPI.collectionName).document(user.uid)).set(user)
+    every { documentReference.set(testUser) } returns APITestUtils.mockSuccessfulTask()
+
+    val successMock = mockk<() -> Unit>(relaxed = true)
+
+    userAPI.addUser(testUser, successMock, { fail("Should not fail") })
+
+    verify { successMock.invoke() }
+
+    every { documentReference.set(testUser) } returns APITestUtils.mockFailingTask()
+
+    val failureMock = mockk<(Exception) -> Unit>(relaxed = true)
+
+    userAPI.addUser(testUser, { fail("Should not succeed") }, failureMock)
+
+    verify { failureMock.invoke(any()) }
   }
 
   @Test
   fun testDeleteUser() {
-    Mockito.`when`(db.collection(Mockito.any())).thenReturn(collectionReference)
-    Mockito.`when`(db.collection(Mockito.any()).document(Mockito.any()))
-        .thenReturn(documentReference)
-    Mockito.`when`(documentReference.delete()).thenReturn(Tasks.forResult(null))
-    Tasks.await(userAPI.deleteUser(user.uid))
-    Mockito.verify(db).collection(userAPI.collectionName)
-    Mockito.verify(db.collection(userAPI.collectionName)).document(user.uid)
-    Mockito.verify(db.collection(userAPI.collectionName).document(user.uid)).delete()
+    every { documentReference.delete() } returns APITestUtils.mockSuccessfulTask()
+
+    val successMock = mockk<() -> Unit>(relaxed = true)
+
+    userAPI.deleteUser(testUser.uid, successMock, { fail("Should not fail") })
+
+    verify { successMock.invoke() }
+
+    every { documentReference.delete() } returns APITestUtils.mockFailingTask()
+
+    val failureMock = mockk<(Exception) -> Unit>(relaxed = true)
+
+    userAPI.deleteUser(testUser.uid, { fail("Should not succeed") }, failureMock)
+
+    verify { failureMock.invoke(any()) }
   }
 }
-*/
