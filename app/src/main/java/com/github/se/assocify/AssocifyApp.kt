9722232b139c075083607726d70f8437c3e2dae0
Lorenzo <lorenzo.padrini@epfl.ch>
2024-04-11 18:13:41 +0200

    fix: removed modifications that were for test

--- app/src/main/java/com/github/se/assocify/AssocifyApp.kt
+++ app/src/main/java/com/github/se/assocify/AssocifyApp.kt
@@ -1,11 +1,11 @@
 package com.github.se.assocify
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
-import com.github.se.assocify.model.database.AssociationAPI
-import com.github.se.assocify.model.database.UserAPI
+import com.github.se.assocify.navigation.Destination
 import com.github.se.assocify.navigation.NavigationActions
-import com.github.se.assocify.ui.screens.selectAssoc.SelectAssociation
+import com.github.se.assocify.navigation.mainNavGraph
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
 
@@ -13,11 +13,8 @@ import com.google.firebase.firestore.firestore
 fun AssocifyApp() {
   val navController = rememberNavController()
   val navActions = NavigationActions(navController)
-  val db = Firebase.firestore /*
+  val db = Firebase.firestore
   NavHost(navController = navController, startDestination = Destination.Home.route) {
     mainNavGraph(navActions = navActions)
   }
-  */
-  SelectAssociation(
-      navActions = navActions, associationAPI = AssociationAPI(db), userAPI = UserAPI(db))
 }
