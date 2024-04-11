3caa7521fc274eadbcf07fd07fd588959288dac9
Mai-LinhC <mai-linh.cordonnier@epfl.ch>
2024-04-11 14:33:18 +0200

    feat: adding hadRole and toggleRole to the user class

--- app/src/main/java/com/github/se/assocify/model/entities/User.kt
+++ app/src/main/java/com/github/se/assocify/model/entities/User.kt
@@ -2,4 +2,16 @@ package com.github.se.assocify.model.entities
 
 data class User(val uid: String, val name: String, val role: Role) {
   constructor() : this("", "", Role("pending"))
+
+  fun hasRole(role: String): Boolean {
+    return this.role.name.uppercase().contains(role.uppercase())
+  }
+
+  fun toggleRole(role: String) : User {
+    return if (hasRole(role)) {
+      User(uid, name, Role("pending"))
+    } else {
+      User(uid, name, Role(role))
+    }
+  }
 }
