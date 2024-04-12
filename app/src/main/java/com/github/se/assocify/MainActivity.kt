2fc6be8ab7475fbad50ad4ca0cf6740beca4a013
ZiZouet <91062845+zizouet@users.noreply.github.com>
2024-04-12 14:51:40 +0200

    fix: ktfmt

--- app/src/main/java/com/github/se/assocify/MainActivity.kt
+++ app/src/main/java/com/github/se/assocify/MainActivity.kt
@@ -8,10 +8,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.github.se.assocify.ui.theme.AssocifyTheme
-import io.github.jan.supabase.createSupabaseClient
-import io.github.jan.supabase.postgrest.Postgrest
-
-
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
