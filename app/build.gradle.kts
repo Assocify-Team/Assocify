e7d84bdef3e9606eee6918edf1450a09b6826f26
Mai-LinhC <mai-linh.cordonnier@epfl.ch>
2024-04-12 12:00:02 +0200

    build: add material icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,6 +91,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview:1.6.4")
     implementation("androidx.compose.material3:material3:1.2.1")
     implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.compose.material:material-icons-extended")
 
     implementation("androidx.compose.ui:ui:1.6.4")
     implementation("androidx.compose.ui:ui-tooling-preview:1.6.4")
