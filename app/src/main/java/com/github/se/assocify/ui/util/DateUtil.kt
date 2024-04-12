b9def577350ec38b8e4e6515aefa23e3be77a21a
SidonieBouthors <sidonie.bouthors@epfl.ch>
2024-04-12 22:26:44 +0200

    docs: add documentation to price and date util

--- app/src/main/java/com/github/se/assocify/ui/util/DateUtil.kt
+++ app/src/main/java/com/github/se/assocify/ui/util/DateUtil.kt
@@ -2,10 +2,21 @@ package com.github.se.assocify.ui.util
 
 import java.time.format.DateTimeFormatter
 
+/**
+ * Utility class for date conversion.
+ * The date format is dd/MM/yyyy.
+ */
 object DateUtil {
 
+  /**
+   * String representing a null date.
+   */
   const val NULL_DATE_STRING = "--/--/--"
 
+  /**
+   * Converts a date to a string.
+   * If the date is null, it returns a string representing a null date.
+   */
   fun toString(date: java.time.LocalDate?): String {
     if (date == null) {
       return NULL_DATE_STRING
@@ -13,6 +24,10 @@ object DateUtil {
     return date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
   }
 
+  /**
+   * Converts a string to a date.
+   * If the string is empty or represents a null date, it returns null.
+   */
   fun toDate(date: String): java.time.LocalDate? {
     if (date.isEmpty() || date == NULL_DATE_STRING) {
       return null
