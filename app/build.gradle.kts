90e99ef36f4f958b6d1d4c61e3bf06a4a58f2e9b
SidonieBouthors <sidonie.bouthors@epfl.ch>
2024-04-15 18:34:25 +0200

    build: update nav version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -132,7 +132,7 @@ dependencies {
     val navVersion = "2.7.7"
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
-    implementation("androidx.navigation:navigation-compose:2.6.0-rc01") // compose
+    implementation("androidx.navigation:navigation-compose:$navVersion") // compose
 
     // Coil (image loading)
     implementation("io.coil-kt:coil-compose:2.0.0")
