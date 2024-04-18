1d381d84f837ae0d72a1cc5f7db7ea90329025d3
Lorenzo <lorenzo.padrini@epfl.ch>
2024-04-18 18:01:34 +0200

    style: ktfmt

--- app/src/main/java/com/github/se/assocify/ui/screens/event/EventScreen.kt
+++ app/src/main/java/com/github/se/assocify/ui/screens/event/EventScreen.kt
@@ -21,9 +21,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.compose.rememberNavController
 import com.github.se.assocify.model.entities.Event
 import com.github.se.assocify.model.entities.Task
 import com.github.se.assocify.navigation.Destination
