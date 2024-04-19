08c39bad96ded620014747a145c17f9c2d646f86
Mai-LinhC <mai-linh.cordonnier@epfl.ch>
2024-04-19 20:18:02 +0200

    feat: basic UI with only text

--- app/src/main/java/com/github/se/assocify/ui/screens/profile/ProfileScreen.kt
+++ app/src/main/java/com/github/se/assocify/ui/screens/profile/ProfileScreen.kt
@@ -1,16 +1,39 @@
 package com.github.se.assocify.ui.screens.profile
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.github.se.assocify.navigation.Destination
 import com.github.se.assocify.navigation.MAIN_TABS_LIST
 import com.github.se.assocify.navigation.NavigationActions
 import com.github.se.assocify.ui.composables.MainNavigationBar
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileScreen(navActions: NavigationActions) {
   Scaffold(
@@ -20,7 +43,101 @@ fun ProfileScreen(navActions: NavigationActions) {
             onTabSelect = { navActions.navigateToMainTab(it) },
             tabList = MAIN_TABS_LIST,
             selectedTab = Destination.Profile)
-      }) {
-        Text(modifier = Modifier.padding(it), text = "Profile Screen")
+      },
+      topBar = {
+          CenterAlignedTopAppBar(
+              title = { Text("Profile") },
+              navigationIcon = {
+                  IconButton(onClick = { /* TODO onAssoClick ?? */ }) {
+                      Icon(
+                          imageVector = Icons.Filled.AccountCircle,
+                          contentDescription = "Association Profile Icon"
+                      )
+                  }
+              }
+          )
+      },
+      contentWindowInsets = WindowInsets(20.dp, 10.dp, 20.dp, 20.dp),
+      ) { innerPadding ->
+          Column(modifier = Modifier.padding(innerPadding), verticalArrangement = Arrangement.spacedBy(16.dp)) {
+
+              Row(
+                  modifier = Modifier
+                      .fillMaxWidth()
+                      .padding(16.dp),
+                  verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+
+              ) {
+                  // profile picture
+                  Text(text = "Profile Picture")
+
+                  Column {
+                        Text("Name")
+                        Text("Role")
+                  }
+
+                  IconButton(onClick = { /*TODO modify profile infos */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Edit,
+                            contentDescription = "Edit Profile Icon"
+                        )
+                  }
+              }
+
+              ExposedDropdownMenuBox(
+                  expanded = false /*expanded*/,
+                  onExpandedChange = {
+                      /*expanded = !expanded*/
+                  },
+                  modifier = Modifier.fillMaxWidth()
+              ) {
+                  TextField(
+                      value = "oui"/*selectedText*/,
+                      onValueChange = {},
+                      readOnly = true,
+                      trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = false/*expanded*/) },
+                      modifier = Modifier
+                          .menuAnchor()
+                          .fillMaxWidth()
+                  )
+
+                  ExposedDropdownMenu(
+                      expanded = false/*expanded*/,
+                      onDismissRequest = { /*expanded = false*/ }
+                  ) {/*
+                      coffeeDrinks.forEach { item ->
+                          DropdownMenuItem(
+                              text = { Text(text = item) },
+                              onClick = {
+                                  selectedText = item
+                                  expanded = false
+                                  Toast.makeText(context, item, Toast.LENGTH_SHORT).show()
+                              }
+                          )
+                      }*/
+                  }
+              }
+
+              Text(text = "Settings")
+
+              Column {
+                ListItem(headlineContent = { Text(text = "Theme") })
+                ListItem(headlineContent = { Text(text = "Privacy/Security") })
+                ListItem(headlineContent = { Text(text = "Notifications") })
+              }
+
+              Text(text = "Manage ${"Association"}")
+
+              Column {
+                ListItem(headlineContent = { Text(text = "Members") })
+                ListItem(headlineContent = { Text(text = "Roles") })
+              }
+
+              TextButton(onClick = { /*TODO*/ }, modifier = Modifier.fillMaxWidth()) {
+                  Text(text = "Log out", textAlign = TextAlign.Center)
+              }
+
+          }
       }
 }
