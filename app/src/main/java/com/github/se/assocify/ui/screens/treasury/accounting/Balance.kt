6cc39fa13179d22b94533815b2fd8a832706e269
Sarah Badr <92681618+sarahbadr17@users.noreply.github.com>
2024-04-19 13:55:04 +0200

    feat: created budget and balance composable

--- app/src/main/java/com/github/se/assocify/ui/screens/treasury/accounting/Balance.kt
+++ app/src/main/java/com/github/se/assocify/ui/screens/treasury/accounting/Balance.kt
@@ -1,2 +1,8 @@
 package com.github.se.assocify.ui.screens.treasury.accounting
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Balance(){
+
+}
--- app/src/main/java/com/github/se/assocify/ui/screens/treasury/accounting/Budget.kt
+++ app/src/main/java/com/github/se/assocify/ui/screens/treasury/accounting/Budget.kt
@@ -1,2 +1,7 @@
 package com.github.se.assocify.ui.screens.treasury.accounting
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Budget() {
+}
