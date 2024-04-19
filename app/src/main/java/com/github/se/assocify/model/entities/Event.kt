ab34f681a4fe981c107d0c24326777c418332de1
ZiZouet <91062845+zizouet@users.noreply.github.com>
2024-04-19 16:13:22 +0200

    feat: documentation Event

--- app/src/main/java/com/github/se/assocify/model/entities/Event.kt
+++ app/src/main/java/com/github/se/assocify/model/entities/Event.kt
@@ -2,7 +2,15 @@ package com.github.se.assocify.model.entities
 
 import kotlinx.datetime.LocalDate
 import kotlinx.serialization.Serializable
-
+/**
+ * @param uid unique identifier of the event
+ * @param name name of the event
+ * @param description description of the event
+ * @param startDate start date of the event
+ * @param endDate end date of the event
+ * @param guestsOrArtists guests or artists of the event
+ * @param location location of the event
+*/
 @Serializable
 data class Event(
     val uid: String,
