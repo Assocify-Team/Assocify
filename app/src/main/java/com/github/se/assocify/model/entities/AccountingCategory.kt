55c4df6eb694ec4d1d27589b722763757c5f1417
Sarah Badr <92681618+sarahbadr17@users.noreply.github.com>
2024-04-22 22:42:08 +0200

    feat: accountingSubCategory and accountingcategory

--- app/src/main/java/com/github/se/assocify/model/entities/AccountingCategory.kt
+++ app/src/main/java/com/github/se/assocify/model/entities/AccountingCategory.kt
@@ -1,10 +1,11 @@
 package com.github.se.assocify.model.entities
 
 /** Represents the category of an accounting item. */
+// TODO: this should be in DB
+// admin or presidence should be able to add new categories or modify it
 enum class AccountingCategory {
   POLE,
   EVENT,
-  PROJECT,
-  COMMISSION,
-  OTHER
+  FEES,
+  COMMISSION
 }
--- app/src/main/java/com/github/se/assocify/model/entities/AccountingSubCategory.kt
+++ app/src/main/java/com/github/se/assocify/model/entities/AccountingSubCategory.kt
@@ -1,4 +1,19 @@
 package com.github.se.assocify.model.entities
 
-/** Represents the subcategory of an accounting item which is related to a category. */
+/**
+ * Represents the subcategory of an accounting item which is related to a category. For example,
+ * Logistic Pole is a subcategory of the Pole category
+ */
 data class AccountingSubCategory(val name: String, val category: AccountingCategory)
+
+// TODO: list of accountingSubcategory should be in db:
+// admin or presidence should be able to add new subcategories or modify it
+val accountingSubCategories =
+    listOf(
+        AccountingSubCategory("Game", AccountingCategory.POLE),
+        AccountingSubCategory("Logistic Pole", AccountingCategory.POLE),
+        AccountingSubCategory("Communication Pole", AccountingCategory.POLE),
+        AccountingSubCategory("ICBD", AccountingCategory.EVENT),
+        AccountingSubCategory("SDF", AccountingCategory.EVENT),
+        AccountingSubCategory("Game*", AccountingCategory.COMMISSION),
+        AccountingSubCategory("Financial Fees", AccountingCategory.FEES))
