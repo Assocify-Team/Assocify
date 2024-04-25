96102c894a3df7c7dc2df4c09da58b06a07a8ddb
SidonieBouthors <sidonie.bouthors@epfl.ch>
2024-04-25 16:18:30 +0200

    refactor: organize packages

--- app/src/main/java/com/github/se/assocify/ui/screens/treasury/receiptstab/receipt/ReceiptViewModel.kt
+++ app/src/main/java/com/github/se/assocify/ui/screens/treasury/receiptstab/receipt/ReceiptViewModel.kt
@@ -1,4 +1,4 @@
-package com.github.se.assocify.ui.screens.treasury.receipt
+package com.github.se.assocify.ui.screens.treasury.receiptstab.receipt
 
 import android.net.Uri
 import androidx.compose.material3.SnackbarDuration
