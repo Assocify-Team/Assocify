692e36f0bdcc92d3d8ad2896ea97ec3e54a8a2f1
SidonieBouthors <sidonie.bouthors@epfl.ch>
2024-04-25 20:42:42 +0200

    style: ktfmt

--- app/src/test/java/com/github/se/assocify/utils/TimeUtilTest.kt
+++ app/src/test/java/com/github/se/assocify/utils/TimeUtilTest.kt
@@ -1,26 +1,25 @@
 package com.github.se.assocify.utils
 
 import com.github.se.assocify.ui.util.TimeUtil
+import java.time.LocalTime
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import java.time.LocalTime
 
 class TimeUtilTest {
 
-    @Test
-    fun testFormatTime() {
-        val time = LocalTime.of(16, 40)
-        val expected = "16:40"
-        val actual = TimeUtil.toString(time)
-        assertEquals(expected, actual)
-    }
+  @Test
+  fun testFormatTime() {
+    val time = LocalTime.of(16, 40)
+    val expected = "16:40"
+    val actual = TimeUtil.toString(time)
+    assertEquals(expected, actual)
+  }
 
-    @Test
-    fun testToTime() {
-        val time = "16:40"
-        val expected = LocalTime.of(16, 40)
-        val actual = TimeUtil.toTime(time)
-        assertEquals(expected, actual)
-    }
-
+  @Test
+  fun testToTime() {
+    val time = "16:40"
+    val expected = LocalTime.of(16, 40)
+    val actual = TimeUtil.toTime(time)
+    assertEquals(expected, actual)
+  }
 }
