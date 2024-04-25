327e33b86d60f47806326579c336de130a8c93dc
Mai-LinhC <mai-linh.cordonnier@epfl.ch>
2024-04-25 21:32:48 +0200

    feat: modifying view, can change name and picture

--- app/src/main/java/com/github/se/assocify/ui/screens/profile/ProfileScreen.kt
+++ app/src/main/java/com/github/se/assocify/ui/screens/profile/ProfileScreen.kt
@@ -1,14 +1,19 @@
 package com.github.se.assocify.ui.screens.profile
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Arrangement.Start
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
@@ -22,9 +27,11 @@ import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.ManageAccounts
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.People
+import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
@@ -33,24 +40,29 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.github.se.assocify.R
+import androidx.compose.ui.window.Dialog
+import coil.compose.AsyncImage
 import com.github.se.assocify.model.CurrentUser
 import com.github.se.assocify.model.database.AssociationAPI
 import com.github.se.assocify.model.database.UserAPI
@@ -58,6 +70,7 @@ import com.github.se.assocify.navigation.Destination
 import com.github.se.assocify.navigation.MAIN_TABS_LIST
 import com.github.se.assocify.navigation.NavigationActions
 import com.github.se.assocify.ui.composables.MainNavigationBar
+import com.github.se.assocify.ui.composables.PhotoSelectionSheet
 
 /**
  * Profile screen that displays the user's information, a way to change your current association and
@@ -74,15 +87,15 @@ fun ProfileScreen(
     userAPI: UserAPI,
     viewmodel: ProfileViewModel = ProfileViewModel(assoAPI, userAPI)
 ) {
-    // for 'change association' part
+  // for 'change association' part
   val listAsso = listOf("Association1", "Association2", "Association3")
   var expanded by remember { mutableStateOf(false) }
   var selectedText by remember { mutableStateOf(listAsso[0]) }
   var currAsso =
       assoAPI.getAssociation(CurrentUser.associationUid!!, { selectedText = it.name }, {})
-    //
+  //
 
-    val state by viewmodel.uiState.collectAsState()
+  val state by viewmodel.uiState.collectAsState()
 
   Scaffold(
       modifier = Modifier.testTag("profileScreen"),
@@ -110,32 +123,50 @@ fun ProfileScreen(
             Modifier.padding(innerPadding).verticalScroll(rememberScrollState()).fillMaxWidth(),
         verticalArrangement = Arrangement.spacedBy(16.dp)) {
           Row(
-              modifier = Modifier.fillMaxWidth().padding(16.dp),
+              modifier = Modifier.fillMaxWidth(),
               verticalAlignment = Alignment.CenterVertically,
-              horizontalArrangement = Arrangement.SpaceBetween) {
+              horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.Start)) {
 
                 // profile picture
-                Image(
-                    painter = painterResource(id = R.drawable.profile_tab_icon),
-                    contentDescription = null,
-                    modifier =
-                        Modifier.size(56.dp)
-                            .clip(CircleShape) // Clip the image to a circle shape
-                            .testTag("profilePicture"))
 
-                // personal information (depends on current association)
-                Column {
-                  Text(state.myName, modifier = Modifier.testTag("profileName"))
-                  Text("Role", modifier = Modifier.testTag("profileRole"))
+                if (state.receiptImageURI != null) {
+                  AsyncImage(
+                      modifier =
+                          Modifier.size(80.dp)
+                              .clip(CircleShape) // Clip the image to a circle shape
+                              .aspectRatio(1f)
+                              .clickable { viewmodel.showBottomSheet() }
+                              .testTag("profilePicture"),
+                      model = state.receiptImageURI,
+                      contentDescription = "profile picture",
+                      contentScale = ContentScale.Crop)
+                } else {
+                  Image(
+                      modifier = Modifier.size(80.dp).clickable { viewmodel.showBottomSheet() },
+                      imageVector = Icons.Outlined.AccountCircle,
+                      contentDescription = "default profile icon")
                 }
 
-                // edit profile button
-                IconButton(
-                    onClick = { /*TODO modify profile infos */},
-                    modifier = Modifier.testTag("editProfile")) {
-                      Icon(
-                          imageVector = Icons.Filled.Edit, contentDescription = "Edit Profile Icon")
-                    }
+                // personal information (depends on current association)
+                Column(modifier = Modifier.testTag("profileInfos").weight(1f)) {
+                  Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(
+                        state.myName,
+                        modifier = Modifier.testTag("profileName").weight(1f),
+                        style = MaterialTheme.typography.headlineSmall)
+
+                    // edit name button
+                    IconButton(
+                        onClick = { viewmodel.modifyNameOpen() },
+                        modifier = Modifier.testTag("editProfile")) {
+                          Icon(
+                              imageVector = Icons.Filled.Edit,
+                              contentDescription = "Edit Profile Icon")
+                        }
+                  }
+
+                  Text("Role", modifier = Modifier.testTag("profileRole"))
+                }
               }
 
           // Change_association dropdown
@@ -143,23 +174,37 @@ fun ProfileScreen(
               expanded = expanded,
               onExpandedChange = { expanded = !expanded },
               modifier =
-                  Modifier.testTag("associationDropdown").align(Alignment.CenterHorizontally)) {
-                TextField(
+                  Modifier
+                      .testTag("associationDropdown")
+                      .align(Alignment.CenterHorizontally)) {
+                OutlinedTextField(
                     value = selectedText,
                     onValueChange = {},
                     readOnly = true,
                     trailingIcon = {
                       ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
                     },
-                    modifier = Modifier.menuAnchor())
+                    modifier = Modifier.menuAnchor(),
+                    leadingIcon = {
+                      Icon(
+                          imageVector = Icons.Default.People, // todo
+                          contentDescription = "Association Logo")
+                    })
 
-                ExposedDropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+                ExposedDropdownMenu(
+                    modifier = Modifier,
+                    expanded = expanded, onDismissRequest = { expanded = false }) {
                   listAsso.forEach { item ->
                     DropdownMenuItem(
                         text = { Text(text = item) },
                         onClick = {
                           selectedText = item
                           expanded = false
+                        },
+                        leadingIcon = {
+                          Icon(
+                              imageVector = Icons.Default.People, // todo
+                              contentDescription = "Association Logo")
                         })
                   }
                 }
@@ -265,5 +310,39 @@ fun ProfileScreen(
                 Text(text = "Log out", textAlign = TextAlign.Center)
               }
         }
+
+    // open dialog to edit member
+    if (state.openEdit) {
+      Dialog(onDismissRequest = { viewmodel.cancelModifyName() }) {
+        ElevatedCard {
+          Column(
+              modifier = Modifier.padding(16.dp).fillMaxWidth(),
+              horizontalAlignment = Alignment.CenterHorizontally) {
+                OutlinedTextField(
+                    value = state.modifyingName,
+                    onValueChange = { viewmodel.modifyName(it) },
+                    label = { Text("Edit your name") })
+                Row(
+                    modifier = Modifier.fillMaxWidth().padding(4.dp),
+                    horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+                      OutlinedButton(
+                          onClick = { viewmodel.confirmModifyName() },
+                          modifier =
+                              Modifier.wrapContentSize()
+                                  .weight(1f)
+                                  .testTag("confirmModifyButton")) {
+                            Text(text = "Confirm", textAlign = TextAlign.Center)
+                          }
+                    }
+              }
+        }
+      }
+    }
+
+    PhotoSelectionSheet(
+        visible = state.showBottomSheet,
+        hideSheet = { viewmodel.hideBottomSheet() },
+        setImageUri = { viewmodel.setImage(it) },
+        signalCameraPermissionDenied = { viewmodel.signalCameraPermissionDenied() })
   }
 }
