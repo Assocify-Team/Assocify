1129f56bcd00f40803b174a0766bd3c34de5cc54
Mai-LinhC <mai-linh.cordonnier@epfl.ch>
2024-04-25 21:33:01 +0200

    feat: modifying viewmodel, can change name and picture

--- app/src/main/java/com/github/se/assocify/ui/screens/profile/ProfileViewModel.kt
+++ app/src/main/java/com/github/se/assocify/ui/screens/profile/ProfileViewModel.kt
@@ -1,34 +1,80 @@
 package com.github.se.assocify.ui.screens.profile
 
+import android.net.Uri
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHostState
 import androidx.lifecycle.ViewModel
 import com.github.se.assocify.model.CurrentUser
 import com.github.se.assocify.model.database.AssociationAPI
 import com.github.se.assocify.model.database.UserAPI
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class ProfileViewModel(private val assoAPI: AssociationAPI, private val userAPI: UserAPI) :
     ViewModel() {
-    private val _uiState = MutableStateFlow(ProfileUIState())
-    val uiState: StateFlow<ProfileUIState> = _uiState
+  private val _uiState = MutableStateFlow(ProfileUIState())
+  val uiState: StateFlow<ProfileUIState> = _uiState
 
-    init {
-        userAPI.getUser(CurrentUser.userUid!!, { user ->
-            _uiState.value = _uiState.value.copy(myName = user.name)
-        }, {_uiState.value = _uiState.value.copy(myName = "name not found")})
-    }
+  init {
+    userAPI.getUser(
+        CurrentUser.userUid!!,
+        { user -> _uiState.value = _uiState.value.copy(myName = user.name) },
+        { _uiState.value = _uiState.value.copy(myName = "name not found") })
+  }
 
-    fun setName(name: String) {
-    _uiState.value = _uiState.value.copy(myName = name)
+  fun modifyNameOpen() {
+    _uiState.value = _uiState.value.copy(openEdit = true)
+  }
+
+  fun modifyName(name: String) {
+    _uiState.value = _uiState.value.copy(modifyingName = name)
+  }
+
+  fun cancelModifyName() {
+    _uiState.value = _uiState.value.copy(openEdit = false)
+  }
+
+  fun confirmModifyName() {
+    _uiState.value = _uiState.value.copy(openEdit = false)
+    _uiState.value = _uiState.value.copy(myName = _uiState.value.modifyingName)
     // change in DB : unsure if this works
     CurrentUser.userUid?.let { uid ->
-        userAPI.getUser(uid, { user ->
-            userAPI.addUser(user.copy(name = name), {}, {})
-            }, {})
-        }
+      userAPI.getUser(
+          uid,
+          { user -> userAPI.addUser(user.copy(name = _uiState.value.modifyingName), {}, {}) },
+          {})
     }
+  }
+
+  fun showBottomSheet() {
+    _uiState.value = _uiState.value.copy(showBottomSheet = true)
+  }
+
+  fun hideBottomSheet() {
+    _uiState.value = _uiState.value.copy(showBottomSheet = false)
+  }
+
+  fun setImage(uri: Uri?) {
+    if (uri == null) return
+    _uiState.value = _uiState.value.copy(receiptImageURI = uri)
+  }
+
+  fun signalCameraPermissionDenied() {
+    CoroutineScope(Dispatchers.Main).launch {
+      _uiState.value.snackbarHostState.showSnackbar(
+          message = "Camera permission denied", duration = SnackbarDuration.Short)
     }
+  }
+}
 
 data class ProfileUIState(
-    val myName: String = "name",
+    val myName: String = "",
+    val modifyingName: String = myName,
+    val openEdit: Boolean = false,
+    val snackbarHostState: SnackbarHostState = SnackbarHostState(),
+    val showBottomSheet: Boolean = false,
+    val receiptImageURI: Uri? = null
 )
