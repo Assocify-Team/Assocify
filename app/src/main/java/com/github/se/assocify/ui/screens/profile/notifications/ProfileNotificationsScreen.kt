e0efe429afd181f9aa2926cb34c9747e636f1470
Mai-LinhC <mai-linh.cordonnier@epfl.ch>
2024-04-26 14:21:30 +0200

    feat: full nav for notifs

--- app/src/main/java/com/github/se/assocify/ui/screens/profile/notifications/ProfileNotificationsScreen.kt
+++ app/src/main/java/com/github/se/assocify/ui/screens/profile/notifications/ProfileNotificationsScreen.kt
@@ -1,25 +1,36 @@
 package com.github.se.assocify.ui.screens.profile.notifications
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
-import com.github.se.assocify.navigation.Destination
-import com.github.se.assocify.navigation.MAIN_TABS_LIST
 import com.github.se.assocify.navigation.NavigationActions
-import com.github.se.assocify.ui.composables.MainNavigationBar
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileNotificationsScreen(navActions: NavigationActions) {
   Scaffold(
       modifier = Modifier.testTag("Notifications Screen"),
-      bottomBar = {
-        MainNavigationBar(
-            onTabSelect = { navActions.navigateToMainTab(it) },
-            tabList = MAIN_TABS_LIST,
-            selectedTab = Destination.Profile)
+      topBar = {
+        CenterAlignedTopAppBar(
+            title = { Text("Notifications Settings") },
+            navigationIcon = {
+              IconButton(onClick = { navActions.back() }) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = "Arrow Back")
+              }
+            })
       }) {
         Text(modifier = Modifier.padding(it), text = "Notifications Screen : not yet implemented")
       }
