045a428c7b691b9b7ebc89fc4c251b0ddbe7f547
Mai-LinhC <mai-linh.cordonnier@epfl.ch>
2024-04-26 14:24:16 +0200

    feat: full nav for theme

--- app/src/main/java/com/github/se/assocify/ui/screens/profile/theme/ProfileThemeScreen.kt
+++ app/src/main/java/com/github/se/assocify/ui/screens/profile/theme/ProfileThemeScreen.kt
@@ -1,6 +1,12 @@
 package com.github.se.assocify.ui.screens.profile.theme
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,15 +17,21 @@ import com.github.se.assocify.navigation.MAIN_TABS_LIST
 import com.github.se.assocify.navigation.NavigationActions
 import com.github.se.assocify.ui.composables.MainNavigationBar
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileThemeScreen(navActions: NavigationActions) {
   Scaffold(
       modifier = Modifier.testTag("themeScreen"),
-      bottomBar = {
-        MainNavigationBar(
-            onTabSelect = { navActions.navigateToMainTab(it) },
-            tabList = MAIN_TABS_LIST,
-            selectedTab = Destination.Profile)
+      topBar = {
+          CenterAlignedTopAppBar(
+              title = { Text("Theme Settings") },
+              navigationIcon = {
+                  IconButton(onClick = { navActions.back() }) {
+                      Icon(
+                          imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                          contentDescription = "Arrow Back")
+                  }
+              })
       }) {
         Text(modifier = Modifier.padding(it), text = "Theme Screen : not yet implemented")
       }
