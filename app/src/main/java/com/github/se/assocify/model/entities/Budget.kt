5d312c496f36a99baf1a4c70d6ac061b183f353a
ZiZouet <91062845+zizouet@users.noreply.github.com>
2024-04-29 23:51:32 +0200

    feat: documentation

--- app/src/main/java/com/github/se/assocify/model/entities/Budget.kt
+++ app/src/main/java/com/github/se/assocify/model/entities/Budget.kt
@@ -1,6 +1,4 @@
 package com.github.se.assocify.model.entities
 
-/**
- * Represents a budget sheet
- */
+/** Represents a budget sheet */
 data class Budget(val budget: List<BudgetItem>, val name: String, val uid: String)
