31097cbaf9bd887c94a26cf8692fb71de11cd454
ZiZouet <91062845+zizouet@users.noreply.github.com>
2024-04-30 17:16:02 +0200

    fix: coverage

--- app/src/main/java/com/github/se/assocify/model/database/BudgetAPI.kt
+++ app/src/main/java/com/github/se/assocify/model/database/BudgetAPI.kt
@@ -2,17 +2,16 @@ package com.github.se.assocify.model.database
 
 import com.github.se.assocify.model.entities.Budget
 import com.github.se.assocify.model.entities.BudgetItem
-import com.github.se.assocify.model.interfaces.BudgetAPIInterface
 import io.github.jan.supabase.SupabaseClient
 
-class BudgetAPI(val db: SupabaseClient) : BudgetAPIInterface, SupabaseApi() {
+class BudgetAPI(val db: SupabaseClient) : SupabaseApi() {
   /**
    * Get the budgets of an association
    *
    * @param associationUID the unique identifier of the association
    * @return the list of budgets of the association
    */
-  override fun getBudgets(
+  fun getBudgets(
       associationUID: String,
       onSuccess: (List<Budget>) -> Unit,
       onFailure: (Exception) -> Unit
@@ -25,7 +24,7 @@ class BudgetAPI(val db: SupabaseClient) : BudgetAPIInterface, SupabaseApi() {
    * @param budgetUID the unique identifier of the association
    * @param budgetItem the budget item to add
    */
-  override fun addBudgetItem(
+  fun addBudgetItem(
       budgetUID: String,
       budgetItem: BudgetItem,
       onSuccess: () -> Unit,
@@ -39,7 +38,7 @@ class BudgetAPI(val db: SupabaseClient) : BudgetAPIInterface, SupabaseApi() {
    * @param associationUID the unique identifier of the association
    * @param budget the budget to add
    */
-  override fun addBudget(
+  fun addBudget(
       associationUID: String,
       budget: Budget,
       onSuccess: () -> Unit,
@@ -53,7 +52,7 @@ class BudgetAPI(val db: SupabaseClient) : BudgetAPIInterface, SupabaseApi() {
    * @param associationUID the unique identifier of the association
    * @param budgetItem the budget item to update
    */
-  override fun updateBudgetItem(
+  fun updateBudgetItem(
       associationUID: String,
       budgetItem: BudgetItem,
       onSuccess: () -> Unit,
@@ -67,7 +66,7 @@ class BudgetAPI(val db: SupabaseClient) : BudgetAPIInterface, SupabaseApi() {
    * @param associationUID the unique identifier of the association
    * @param budget the budget to update
    */
-  override fun updateBudget(
+  fun updateBudget(
       associationUID: String,
       budget: Budget,
       onSuccess: () -> Unit,
@@ -80,7 +79,7 @@ class BudgetAPI(val db: SupabaseClient) : BudgetAPIInterface, SupabaseApi() {
    *
    * @param budgetItemUID the unique identifier of the budget item
    */
-  override fun deleteBudgetItem(
+  fun deleteBudgetItem(
       budgetItemUID: String,
       onSuccess: () -> Unit,
       onFailure: (Exception) -> Unit
@@ -92,7 +91,7 @@ class BudgetAPI(val db: SupabaseClient) : BudgetAPIInterface, SupabaseApi() {
    *
    * @param budgetUID the unique identifier of the budget
    */
-  override fun deleteBudget(
+  fun deleteBudget(
       budgetUID: String,
       onSuccess: () -> Unit,
       onFailure: (Exception) -> Unit
