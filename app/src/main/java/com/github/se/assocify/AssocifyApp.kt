d2ec4d9447a22d6e6b5fa27fdb3a3c6e3efa0bda
SidonieBouthors <sidonie.bouthors@epfl.ch>
2024-05-01 17:42:51 +0200

    feat: check saved on startup

--- app/src/main/java/com/github/se/assocify/AssocifyApp.kt
+++ app/src/main/java/com/github/se/assocify/AssocifyApp.kt
@@ -3,26 +3,39 @@ package com.github.se.assocify
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
+import com.github.se.assocify.model.CurrentUser
 import com.github.se.assocify.model.SupabaseClient
 import com.github.se.assocify.model.database.AssociationAPI
 import com.github.se.assocify.model.database.EventAPI
 import com.github.se.assocify.model.database.UserAPI
+import com.github.se.assocify.model.localsave.LoginSave
 import com.github.se.assocify.navigation.Destination
 import com.github.se.assocify.navigation.NavigationActions
 import com.github.se.assocify.navigation.mainNavGraph
 
 @Composable
-fun AssocifyApp() {
+fun AssocifyApp(loginSaver: LoginSave) {
   val navController = rememberNavController()
   val navActions = NavigationActions(navController)
   val userAPI = UserAPI(SupabaseClient.supabaseClient)
   val associationAPI = AssociationAPI(SupabaseClient.supabaseClient)
   val eventAPI = EventAPI(SupabaseClient.supabaseClient)
-  NavHost(navController = navController, startDestination = Destination.Login.route) {
+
+  CurrentUser.userUid = loginSaver.getSavedUserUid()
+  CurrentUser.associationUid = loginSaver.getSavedAssociationUid()
+
+  val firstDest = if (CurrentUser.userUid != null && CurrentUser.associationUid != null) {
+    Destination.Home.route
+  } else {
+    Destination.Login.route
+  }
+
+  NavHost(navController = navController, startDestination = firstDest) {
     mainNavGraph(
         navActions = navActions,
         userAPI = userAPI,
         associationAPI = associationAPI,
-        eventAPI = eventAPI)
+        eventAPI = eventAPI,
+        loginSaver = loginSaver)
   }
 }
--- app/src/main/java/com/github/se/assocify/MainActivity.kt
+++ app/src/main/java/com/github/se/assocify/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.github.se.assocify.model.localsave.LoginSave
 import com.github.se.assocify.ui.theme.AssocifyTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,7 +17,7 @@ class MainActivity : ComponentActivity() {
       AssocifyTheme {
         // A surface container using the 'background' color from the theme
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-          AssocifyApp()
+          AssocifyApp(LoginSave(this))
         }
       }
     }
