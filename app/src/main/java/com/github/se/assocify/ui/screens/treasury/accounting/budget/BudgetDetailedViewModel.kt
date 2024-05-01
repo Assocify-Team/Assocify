a2a36a76278255d940f05f71f952a6a7d7a6b325
Sarah Badr <92681618+sarahbadr17@users.noreply.github.com>
2024-05-01 14:07:23 +0200

    feat: getBudgetItems

--- app/src/main/java/com/github/se/assocify/ui/screens/treasury/accounting/budget/BudgetDetailedViewModel.kt
+++ app/src/main/java/com/github/se/assocify/ui/screens/treasury/accounting/budget/BudgetDetailedViewModel.kt
@@ -1,6 +1,9 @@
 package com.github.se.assocify.ui.screens.treasury.accounting.budget
 
 import androidx.lifecycle.ViewModel
+import com.github.se.assocify.model.CurrentUser
+import com.github.se.assocify.model.database.BudgetAPI
+import com.github.se.assocify.model.entities.AccountingSubCategory
 import com.github.se.assocify.model.entities.BudgetItem
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -11,24 +14,42 @@ import kotlinx.coroutines.flow.StateFlow
  * @param budgetApi the budget api
  */
 class BudgetDetailedViewModel(
-   // private var budgetApi: BudgetAPI
+   private var budgetApi: BudgetAPI
 ) : ViewModel() {
+    private val _uiState: MutableStateFlow<BudgetItemState> =
+        MutableStateFlow(BudgetItemState())
+    val uiState: StateFlow<BudgetItemState>
+
+    init {
+        uiState = _uiState
+        updateDatabaseValues()
+    }
+
+    private fun updateDatabaseValues() {
+        budgetApi.getBudget(CurrentUser.associationUid!!,
+            { budgetList -> _uiState.value = _uiState.value.copy(budgetList = budgetList) }, {}
+        )
+    }
+
+    /** Gets the list of budgetItems of a given subCategory
+     *  @param accountingSubCategory the subCategory to filter on
+     *  @return the list of budgetItems of the given subCategory
+     * */
+
+    fun getBudgetItems(accountingSubCategory: AccountingSubCategory): List<BudgetItem> {
+      return uiState.value.budgetList.filter { item ->
+            item.category == accountingSubCategory
+      }
+    }
 
     //TODO: handle filter
 
     //TODO: handle accoutingSubCategory
 
-    //TODO: getBudgetItems
 
-    //TODO: updateBudgetItems
-
-    //TODO: deleteBudgetItems
 }
 
 
 data class BudgetItemState(
-    val budgetList: List<BudgetItem> = emptyList(),
-    val associationUId: String =  "assoc",
-    val searchQuery: String = "",
-    val searchState: Boolean = false
+    val budgetList: List<BudgetItem> = emptyList()
 )
