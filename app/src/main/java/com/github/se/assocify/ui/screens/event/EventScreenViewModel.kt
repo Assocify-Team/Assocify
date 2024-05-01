7a0ad8216b37e2646c214f6d8bc182868fb70f13
Lorenzo <lorenzo.padrini@epfl.ch>
2024-05-01 17:11:43 +0200

    docs: added the docs to the EventViewModel

--- app/src/main/java/com/github/se/assocify/ui/screens/event/EventScreenViewModel.kt
+++ app/src/main/java/com/github/se/assocify/ui/screens/event/EventScreenViewModel.kt
@@ -7,6 +7,12 @@ import com.github.se.assocify.ui.screens.event.tasktab.EventTaskViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
+/**
+ * A ViewModel of the global Event Screen, to manage the filter chips and the search bar
+ *
+ * @param db the Event database to fetch the events
+ * @param taskListViewModel a viewmodel of the screen of the task list
+ */
 class EventScreenViewModel(
     private var db: EventAPI,
     private val taskListViewModel: EventTaskViewModel
@@ -19,6 +25,7 @@ class EventScreenViewModel(
     updateEventsFromDb()
   }
 
+  /** Fetch the events from the database */
   private fun updateEventsFromDb() {
     db.getEvents(
         { events ->
@@ -31,22 +38,45 @@ class EventScreenViewModel(
         })
   }
 
+  /** Setup the filtered events depending on the current filters */
   private fun updateFilteredEvents(events: List<Event>) {
     _uiState.value = _uiState.value.copy(selectedEvents = events.filter { isEventSelected(it) })
   }
 
+  /**
+   * Display or hide the search bar
+   *
+   * @param searching display the searchbar or not
+   */
   fun modifySearchingState(searching: Boolean) {
     _uiState.value = _uiState.value.copy(stateBarDisplay = searching)
   }
 
+  /**
+   * Modify the current search query
+   *
+   * @param query the new query
+   */
   fun modifySearchQuery(query: String) {
     _uiState.value = _uiState.value.copy(searchQuery = query)
   }
 
+  /**
+   * Switch to a different tab
+   *
+   * @param tab the tab we want to switch to
+   */
   fun switchTab(tab: EventPageIndex) {
+    deactivateSearch()
     _uiState.value = _uiState.value.copy(currentTab = tab)
   }
 
+  /**
+   * Select an event so that it is counted on the filters
+   *
+   * @param event the event we want to select
+   * @param selected whether we want to select or unselect the event
+   */
   fun setEventSelection(event: Event, selected: Boolean) {
     val selectedEvents =
         if (selected) {
@@ -58,14 +88,21 @@ class EventScreenViewModel(
     taskListViewModel.setEvents(selectedEvents)
   }
 
+  /**
+   * Whether the given event is selected or not
+   *
+   * @param event the event for which we want to know if it selected
+   */
   fun isEventSelected(event: Event): Boolean {
     return _uiState.value.selectedEvents.contains(event)
   }
 
+  /** Stop performing the search and hide the searchbar */
   fun deactivateSearch() {
     when (_uiState.value.currentTab) {
       EventPageIndex.TASKS -> {
         taskListViewModel.search("")
+        _uiState.value = _uiState.value.copy(searchQuery = "")
         modifySearchingState(false)
       }
       EventPageIndex.MAP -> {
@@ -77,6 +114,7 @@ class EventScreenViewModel(
     }
   }
 
+  /** Filter the elements from the current tab depending on the current search query */
   fun searchTaskLists() {
     when (_uiState.value.currentTab) {
       EventPageIndex.TASKS -> {
@@ -92,7 +130,11 @@ class EventScreenViewModel(
   }
 }
 
-// Index of each tag for navigation
+/**
+ * The page index of the current event
+ *
+ * @param index the index of the event
+ */
 enum class EventPageIndex(val index: Int) {
   TASKS(0),
   MAP(1),
@@ -103,6 +145,17 @@ enum class EventPageIndex(val index: Int) {
   }
 }
 
+/**
+ * The state of the event screen
+ *
+ * @param searchQuery the current search query
+ * @param stateBarDisplay whether the search bar is displayed or not
+ * @param events the list of events
+ * @param selectedEvents the list of selected events
+ * @param currentTab the current tab
+ * @param error whether an error occurred
+ * @param errorText the error message
+ */
 data class EventScreenState(
     val searchQuery: String = "",
     val stateBarDisplay: Boolean = false,
