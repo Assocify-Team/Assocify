65ce48f90352c15f5d2f6f425b3d6b1435926277
rayan <rayan.boucheny@epfl.ch>
2024-05-06 21:01:16 +0200

    fix: ktfmt

--- app/src/main/java/com/github/se/assocify/ui/screens/event/maptab/EventMapScreen.kt
+++ app/src/main/java/com/github/se/assocify/ui/screens/event/maptab/EventMapScreen.kt
@@ -1,6 +1,5 @@
 package com.github.se.assocify.ui.screens.event.maptab
 
-import android.content.Context
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
@@ -14,7 +13,6 @@ import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleObserver
-import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.MapTileProviderBasic
 import org.osmdroid.tileprovider.tilesource.OnlineTileSourceBase
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory.DEFAULT_TILE_SOURCE
@@ -31,24 +29,17 @@ private const val INITIAL_ZOOM = 15.0
 /** A screen that displays a map of the event: location with the associated tasks. */
 @Composable
 fun EventMapScreen() {
-  Column(
-      modifier =
-      Modifier
-        .fillMaxWidth()
-        .testTag("OSMMapScreen")) {
-        //EventMapView()
-        EPFLMapView(
-            modifier = Modifier
-              .fillMaxWidth())
-      }
+  Column(modifier = Modifier.fillMaxWidth().testTag("OSMMapScreen")) {
+    // EventMapView()
+    EPFLMapView(modifier = Modifier.fillMaxWidth())
+  }
 }
 
 /**
- * Initialize the map view with the EPFL plan tiles and it's lifecycle
- * in order to save its state
+ * Initialize the map view with the EPFL plan tiles and it's lifecycle in order to save its state
  */
 @Composable
-fun rememberMapViewWithLifecycle() : MapView {
+fun rememberMapViewWithLifecycle(): MapView {
   val context = LocalContext.current
 
   // Initialise the map view
@@ -76,53 +67,43 @@ fun rememberMapViewWithLifecycle() : MapView {
   val lifecycle = LocalLifecycleOwner.current.lifecycle
   DisposableEffect(lifecycle) {
     lifecycle.addObserver(lifecycleObserver)
-    onDispose {
-      lifecycle.removeObserver(lifecycleObserver)
-    }
+    onDispose { lifecycle.removeObserver(lifecycleObserver) }
   }
 
   return mapView
 }
 
 /**
- * Observer in order to manage events of the map
- * Useful to save the map, its components and its state
+ * Observer in order to manage events of the map Useful to save the map, its components and its
+ * state
+ *
  * @param mapView the map view to observe
  */
 @Composable
-fun rememberLifecycleObserver(mapView: MapView) : LifecycleObserver =
-  remember(mapView) {
-    LifecycleEventObserver { _, event ->
-      when (event) {
-        Lifecycle.Event.ON_RESUME -> mapView.onResume()
-        Lifecycle.Event.ON_PAUSE -> mapView.onPause()
-        Lifecycle.Event.ON_DESTROY -> mapView.onPause()
-        else -> {}
+fun rememberLifecycleObserver(mapView: MapView): LifecycleObserver =
+    remember(mapView) {
+      LifecycleEventObserver { _, event ->
+        when (event) {
+          Lifecycle.Event.ON_RESUME -> mapView.onResume()
+          Lifecycle.Event.ON_PAUSE -> mapView.onPause()
+          Lifecycle.Event.ON_DESTROY -> mapView.onPause()
+          else -> {}
+        }
       }
     }
-  }
 
-/**
- * A composable that displays a map view.
- */
+/** A composable that displays a map view. */
 @Composable
-fun EPFLMapView(
-  modifier: Modifier,
-  onLoad: ((map: MapView) -> Unit)? = null
-) {
+fun EPFLMapView(modifier: Modifier, onLoad: ((map: MapView) -> Unit)? = null) {
   val mapViewState = rememberMapViewWithLifecycle()
 
   AndroidView(
-    factory = { mapViewState },
-    modifier = modifier,
-    update = { view ->
-      onLoad?.invoke(view)
-    }
-  )
+      factory = { mapViewState }, modifier = modifier, update = { view -> onLoad?.invoke(view) })
 }
 
 /**
  * The custom tile source from the EPFL plan API.
+ *
  * @param floorId the floor id of the map to display
  */
 class CampusTileSource(private val floorId: Int) :
