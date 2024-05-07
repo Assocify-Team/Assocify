bb830960cdc811da68ef714e2fc9610835328c2e
Sarah Badr <92681618+sarahbadr17@users.noreply.github.com>
2024-05-07 10:19:17 +0200

    fix: correct java doc for categories

--- app/src/main/java/com/github/se/assocify/model/entities/AccountingCategory.kt
+++ app/src/main/java/com/github/se/assocify/model/entities/AccountingCategory.kt
@@ -3,6 +3,7 @@ package com.github.se.assocify.model.entities
 /**
  * Represents the category of an accounting item.
  *
+ * @param uid unique identifier of the category
  * @param name name of the category (ex: Pole)
  */
 
--- app/src/main/java/com/github/se/assocify/model/entities/AccountingSubCategory.kt
+++ app/src/main/java/com/github/se/assocify/model/entities/AccountingSubCategory.kt
@@ -4,8 +4,8 @@ package com.github.se.assocify.model.entities
  * Represents the subcategory of an accounting item which is related to a category. For example,
  * Logistic Pole is a subcategory of the Pole category
  *
+ * @param uid unique identifier of the subcategory
  * @param name name of the subcategory (ex: Logistics Pole)
- * @param category category of the subcategory (ex: Pole)
  * @param amount total amount of the subcategory
  */
 data class AccountingSubCategory(val uid: String, val name: String, val amount: Int)
