4b7029100735dc584c696b714f687a82b1ee7891
Sarah Badr <92681618+sarahbadr17@users.noreply.github.com>
2024-05-10 22:05:11 +0200

    docs: java doc for BalanceDetailedViewModel

--- app/src/main/java/com/github/se/assocify/ui/screens/treasury/accounting/balance/BalanceDetailedViewModel.kt
+++ app/src/main/java/com/github/se/assocify/ui/screens/treasury/accounting/balance/BalanceDetailedViewModel.kt
@@ -9,6 +9,12 @@ import java.time.LocalDate
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
+/**
+ * View model for the balance detailed screen
+ *
+ * @param balanceApi the balance api
+ * @param subCategoryUid the subcategory uid
+ */
 class BalanceDetailedViewModel(
     private var balanceApi: BalanceAPI,
     private var subCategoryUid: String
@@ -21,6 +27,7 @@ class BalanceDetailedViewModel(
     uiState = _uiState
   }
 
+  /** Update the database values */
   private fun updateDatabaseValues() {
     balanceApi.getBalance(
         CurrentUser.associationUid!!,
@@ -39,17 +46,34 @@ class BalanceDetailedViewModel(
         {})
   }
 
+  /**
+   * Handle the year filter
+   *
+   * @param year the year to filter by
+   */
   fun onYearFilter(year: Int) {
     _uiState.value = _uiState.value.copy(year = year)
     updateDatabaseValues()
   }
 
+  /**
+   * Handle the status filter
+   *
+   * @param status the status to filter by
+   */
   fun onStatusFilter(status: Status) {
     _uiState.value = _uiState.value.copy(status = status)
     updateDatabaseValues()
   }
 }
 
+/**
+ * The state for the balance item
+ *
+ * @param balanceList the current list of balance items
+ * @param status the current status
+ * @param year the current year
+ */
 data class BalanceItemState(
     val balanceList: List<BalanceItem> = emptyList(),
     val status: Status = Status.Pending,
