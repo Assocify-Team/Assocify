a05263c26f566a29dba5b98ff98cf7d603479d53
SidonieBouthors <sidonie.bouthors@epfl.ch>
2024-05-10 15:08:25 +0200

    feat: circular indicator and error message

---
+++ app/src/main/java/com/github/se/assocify/ui/composables/CenteredCircularIndicator.kt
@@ -0,0 +1,20 @@
+package com.github.se.assocify.ui.composables
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CenteredCircularIndicator() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        CircularProgressIndicator()
+    }
+}
---
+++ app/src/main/java/com/github/se/assocify/ui/composables/RetryError.kt
@@ -0,0 +1,31 @@
+package com.github.se.assocify.ui.composables
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ErrorMessage(
+    errorMessage: String?,
+    onRetry: (() -> Unit)? = null
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        errorMessage?.let {
+            Text(text = it)
+        }
+        onRetry?.let {
+            Button(onClick = it) {
+                Text(text = "Retry")
+            }
+        }
+    }
+}
