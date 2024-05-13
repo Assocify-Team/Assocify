81c9c828e3570833d3712142c7938b2b48af37be
SidonieBouthors <sidonie.bouthors@epfl.ch>
2024-05-13 20:16:07 +0200

    test: fix schedule

--- app/src/androidTest/java/com/github/se/assocify/screens/ScheduleScreenTest.kt
+++ app/src/androidTest/java/com/github/se/assocify/screens/ScheduleScreenTest.kt
@@ -3,10 +3,13 @@ package com.github.se.assocify.screens
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.se.assocify.model.database.TaskAPI
 import com.github.se.assocify.ui.screens.event.scheduletab.EventScheduleScreen
+import com.github.se.assocify.ui.screens.event.scheduletab.EventScheduleViewModel
 import com.kaspersky.components.composesupport.config.withComposeSupport
 import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import io.mockk.mockk
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 import org.junit.Before
@@ -17,6 +20,9 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class ScheduleScreenTest : TestCase(kaspressoBuilder = Kaspresso.Builder.withComposeSupport()) {
   @get:Rule val composeTestRule = createComposeRule()
+
+  val taskAPI: TaskAPI = mockk<TaskAPI>()
+  val viewModel = EventScheduleViewModel(taskAPI)
 
   @Before
   fun testSetup() {
