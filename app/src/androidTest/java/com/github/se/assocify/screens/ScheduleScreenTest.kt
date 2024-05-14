2c5ddf063713564309863ce69b3bc89291126203
SidonieBouthors <sidonie.bouthors@epfl.ch>
2024-05-14 11:53:28 +0200

    test: schedule task

--- app/src/androidTest/java/com/github/se/assocify/screens/ScheduleScreenTest.kt
+++ app/src/androidTest/java/com/github/se/assocify/screens/ScheduleScreenTest.kt
@@ -4,6 +4,7 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.se.assocify.model.database.TaskAPI
+import com.github.se.assocify.model.entities.Event
 import com.github.se.assocify.model.entities.Task
 import com.github.se.assocify.ui.screens.event.scheduletab.EventScheduleScreen
 import com.github.se.assocify.ui.screens.event.scheduletab.EventScheduleViewModel
@@ -12,18 +13,42 @@ import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
 import io.mockk.every
 import io.mockk.mockk
-import java.time.LocalTime
-import java.time.format.DateTimeFormatter
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.time.LocalTime
+import java.time.OffsetDateTime
+import java.time.format.DateTimeFormatter
 
 @RunWith(AndroidJUnit4::class)
 class ScheduleScreenTest : TestCase(kaspressoBuilder = Kaspresso.Builder.withComposeSupport()) {
   @get:Rule val composeTestRule = createComposeRule()
 
-  private val tasks: List<Task> = listOf()
+  private val tasks: List<Task> =
+      listOf(
+          Task(
+              "1",
+              "Task 1",
+              "Event 1",
+              false,
+              OffsetDateTime.now(),
+              5,
+              "Category",
+              "Location",
+              "eventUid"))
+
+    private val events: List<Event> =
+      listOf(
+          Event(
+              "eventUid",
+              "Event 1",
+              "Event 1",
+              OffsetDateTime.now(),
+              OffsetDateTime.now(),
+              "Guests?",
+              "BC")
+      )
 
   private val taskAPI: TaskAPI =
       mockk<TaskAPI>() {
@@ -34,6 +59,7 @@ class ScheduleScreenTest : TestCase(kaspressoBuilder = Kaspresso.Builder.withCom
   @Before
   fun testSetup() {
     composeTestRule.setContent { EventScheduleScreen(viewModel) }
+      viewModel.setEvents(events)
   }
 
   @Test
@@ -43,6 +69,14 @@ class ScheduleScreenTest : TestCase(kaspressoBuilder = Kaspresso.Builder.withCom
         onNodeWithText(LocalTime.of(i, 0).format(DateTimeFormatter.ofPattern("HH:mm")))
             .assertExists()
       }
+    }
+  }
+
+  @Test
+  fun display() {
+    with(composeTestRule) {
+      onNodeWithText("Today").assertExists()
+      onNodeWithText("Task 1").assertExists()
     }
   }
 }
