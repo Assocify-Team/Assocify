0ae21c5a1c170bccf7b6b3918e0896cdc63af716
Lorenzo <lorenzo.padrini@epfl.ch>
2024-05-15 17:37:00 +0200

    feat: added back arrow

--- app/src/main/java/com/github/se/assocify/ui/screens/treasury/accounting/balance/DisplayEditBalance.kt
+++ app/src/main/java/com/github/se/assocify/ui/screens/treasury/accounting/balance/DisplayEditBalance.kt
@@ -8,12 +8,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -61,7 +66,12 @@ fun DisplayEditBalance(balanceDetailedViewModel: BalanceDetailedViewModel) {
     ) {
       LazyColumn {
         item {
-          Text("Edit Balance Item", fontSize = 20.sp, modifier = Modifier.padding(16.dp))
+          Row{
+            IconButton(onClick = { balanceDetailedViewModel.cancelEditing() }) {
+              Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack, contentDescription = null)
+            }
+            Text("Edit Balance Item", fontSize = 20.sp, modifier = Modifier.padding(16.dp))
+          }
         }
         // The name box
         item {
@@ -301,4 +311,3 @@ fun DisplayEditBalance(balanceDetailedViewModel: BalanceDetailedViewModel) {
   }
 }
 
-  
