ca738fa2ac8d10a296c8a01f748398394201209b
rayan <rayan.boucheny@epfl.ch>
2024-05-15 23:00:12 +0200

    fix: ktfmt fix

--- app/src/main/java/com/github/se/assocify/ui/screens/event/EventScreenViewModel.kt
+++ app/src/main/java/com/github/se/assocify/ui/screens/event/EventScreenViewModel.kt
@@ -6,8 +6,8 @@ import androidx.lifecycle.ViewModel
 import com.github.se.assocify.model.database.EventAPI
 import com.github.se.assocify.model.database.TaskAPI
 import com.github.se.assocify.model.entities.Event
-import com.github.se.assocify.ui.screens.event.maptab.EventMapViewModel
 import com.github.se.assocify.navigation.NavigationActions
+import com.github.se.assocify.ui.screens.event.maptab.EventMapViewModel
 import com.github.se.assocify.ui.screens.event.scheduletab.EventScheduleViewModel
 import com.github.se.assocify.ui.screens.event.tasktab.EventTaskViewModel
 import kotlinx.coroutines.CoroutineScope
