79efc80438206b161f2ab95af9ac0835bf5d55c7
Sarah Badr <92681618+sarahbadr17@users.noreply.github.com>
2024-05-17 17:13:30 +0200

    test: search in budget and balance ktfmt

--- app/src/androidTest/java/com/github/se/assocify/screens/TreasuryScreenTest.kt
+++ app/src/androidTest/java/com/github/se/assocify/screens/TreasuryScreenTest.kt
@@ -20,8 +20,6 @@ import com.github.se.assocify.model.entities.Receipt
 import com.github.se.assocify.navigation.NavigationActions
 import com.github.se.assocify.ui.screens.treasury.TreasuryScreen
 import com.github.se.assocify.ui.screens.treasury.TreasuryViewModel
-import com.github.se.assocify.ui.screens.treasury.accounting.AccountingPage
-import com.github.se.assocify.ui.screens.treasury.accounting.AccountingScreen
 import com.github.se.assocify.ui.screens.treasury.accounting.AccountingViewModel
 import com.github.se.assocify.ui.screens.treasury.receiptstab.ReceiptListViewModel
 import com.kaspersky.components.composesupport.config.withComposeSupport
@@ -84,14 +82,15 @@ class TreasuryScreenTest : TestCase(kaspressoBuilder = Kaspresso.Builder.withCom
             }
       }
   var receiptListViewModel: ReceiptListViewModel = ReceiptListViewModel(navActions, mockReceiptAPI)
-    lateinit var accountingViewModel: AccountingViewModel
+  lateinit var accountingViewModel: AccountingViewModel
 
   @Before
   fun testSetup() {
     CurrentUser.userUid = "testUser"
     CurrentUser.associationUid = "testAssociation"
-      accountingViewModel = AccountingViewModel(mockAccountingCategoriesAPI, mockAccountingSubCategoryAPI)
-    val viewModel = TreasuryViewModel(navActions, receiptListViewModel, accountingViewModel )
+    accountingViewModel =
+        AccountingViewModel(mockAccountingCategoriesAPI, mockAccountingSubCategoryAPI)
+    val viewModel = TreasuryViewModel(navActions, receiptListViewModel, accountingViewModel)
     composeTestRule.setContent {
       TreasuryScreen(navActions, accountingViewModel, receiptListViewModel, viewModel)
     }
@@ -160,31 +159,31 @@ class TreasuryScreenTest : TestCase(kaspressoBuilder = Kaspresso.Builder.withCom
     }
   }
 
-    @Test
-    fun searchBarInBudgetAndBalance(){
-        with(composeTestRule){
-            onNodeWithTag("budgetTab").assertIsDisplayed()
-            onNodeWithTag("budgetTab").performClick()
+  @Test
+  fun searchBarInBudgetAndBalance() {
+    with(composeTestRule) {
+      onNodeWithTag("budgetTab").assertIsDisplayed()
+      onNodeWithTag("budgetTab").performClick()
 
-            onNodeWithTag("searchIconButton").performClick()
-            onNodeWithTag("searchBar").assertIsDisplayed()
-            onNodeWithTag("searchBar").onChild().assertIsDisplayed()
-            onNodeWithTag("searchBar").onChild().performTextInput("Presidency")
-            onNodeWithTag("searchBar").onChild().assertTextContains("Presidency")
-            onNodeWithTag("searchClearButton").performClick()
-            onNodeWithTag("searchBar").onChild().assertTextContains("")
-            onNodeWithTag("searchBackButton").performClick()
-            onNodeWithTag("searchBar").assertIsNotDisplayed()
+      onNodeWithTag("searchIconButton").performClick()
+      onNodeWithTag("searchBar").assertIsDisplayed()
+      onNodeWithTag("searchBar").onChild().assertIsDisplayed()
+      onNodeWithTag("searchBar").onChild().performTextInput("Presidency")
+      onNodeWithTag("searchBar").onChild().assertTextContains("Presidency")
+      onNodeWithTag("searchClearButton").performClick()
+      onNodeWithTag("searchBar").onChild().assertTextContains("")
+      onNodeWithTag("searchBackButton").performClick()
+      onNodeWithTag("searchBar").assertIsNotDisplayed()
 
-            onNodeWithTag("balanceTab").assertIsDisplayed()
-            onNodeWithTag("balanceTab").performClick()
+      onNodeWithTag("balanceTab").assertIsDisplayed()
+      onNodeWithTag("balanceTab").performClick()
 
-            onNodeWithTag("searchIconButton").performClick()
-            onNodeWithTag("searchBar").onChild().performTextInput("Presidency")
-            onNodeWithTag("searchBar").onChild().assertTextContains("Presidency")
-
-        }
+      onNodeWithTag("searchIconButton").performClick()
+      onNodeWithTag("searchBar").onChild().performTextInput("Presidency")
+      onNodeWithTag("searchBar").onChild().assertTextContains("Presidency")
     }
+  }
+
   @Test
   fun receiptLoading() {
     every { mockReceiptAPI.getAllReceipts(any(), any()) } answers
