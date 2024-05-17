bdf774e8cfc0f59b645a8aabe97aa0781eab7ab2
Mai-LinhC <mai-linh.cordonnier@epfl.ch>
2024-05-17 15:45:08 +0200

    feat: more test for coverage

--- app/src/androidTest/java/com/github/se/assocify/screens/SelectAssociationScreenTest.kt
+++ app/src/androidTest/java/com/github/se/assocify/screens/SelectAssociationScreenTest.kt
@@ -13,6 +13,8 @@ import com.github.se.assocify.model.CurrentUser
 import com.github.se.assocify.model.database.AssociationAPI
 import com.github.se.assocify.model.database.UserAPI
 import com.github.se.assocify.model.entities.Association
+import com.github.se.assocify.model.entities.PermissionRole
+import com.github.se.assocify.model.entities.RoleType
 import com.github.se.assocify.navigation.Destination
 import com.github.se.assocify.navigation.NavigationActions
 import com.github.se.assocify.ui.screens.selectAssociation.DisplayOrganization
@@ -86,7 +88,28 @@ class SelectAssociationTest : TestCase(kaspressoBuilder = Kaspresso.Builder.with
           val associations = listOf(testAssociation)
           onSuccessCallback(associations)
         }
-    every { mockUserAPI.requestJoin(any(), any(), any()) } answers { mockNavActions.onLogin(true) }
+    every { mockAssocAPI.getRoles(any(), any(), any()) } answers
+        {
+          val onSuccessCallback = secondArg<(List<PermissionRole>) -> Unit>()
+          onSuccessCallback(listOf(PermissionRole("testRole", "testAssociation", RoleType.MEMBER)))
+        }
+    every { mockAssocAPI.acceptUser(any(), any(), any(), any()) } answers
+        {
+          val onSuccessCallback = thirdArg<() -> Unit>()
+          onSuccessCallback()
+        }
+    every { mockUserAPI.updateCurrentUserAssociationCache(any(), any()) } answers
+        {
+          val onSuccessCallback =
+              firstArg<(Map<String, Pair<Association, PermissionRole>>) -> Unit>()
+          onSuccessCallback(emptyMap())
+        }
+    every { mockNavActions.backFromSelectAsso() } returns false
+    every { mockUserAPI.requestJoin(any(), any(), any()) } answers
+        {
+          val onSuccessCallback = secondArg<() -> Unit>()
+          onSuccessCallback()
+        }
   }
 
   /** This test checks if the "Create new organization" button is displayed */
