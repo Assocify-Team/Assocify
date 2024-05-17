97e893a1ffcdef69e14ba5b7bb956f70a3f9fd21
Lorenzo <lorenzo.padrini@epfl.ch>
2024-05-17 20:55:46 +0200

    style: kttfmt

--- app/src/main/java/com/github/se/assocify/ui/screens/treasury/accounting/AccountingDetailedScreen.kt
+++ app/src/main/java/com/github/se/assocify/ui/screens/treasury/accounting/AccountingDetailedScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -396,8 +395,7 @@ fun DisplayEditBudget(budgetViewModel: BudgetDetailedViewModel) {
               modifier = Modifier,
               expanded = tvaExtended,
               onDismissRequest = { tvaExtended = false },
-              properties = PopupProperties(focusable = true)
-          ) {
+              properties = PopupProperties(focusable = true)) {
                 TVA.entries.forEach { tva ->
                   DropdownMenuItem(
                       text = { Text(tva.toString()) },
