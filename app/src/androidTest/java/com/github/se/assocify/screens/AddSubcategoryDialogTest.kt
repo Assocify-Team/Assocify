8f4eba55582452eb3ab4ed12725a8eaa610d2728
SidonieBouthors <sidonie.bouthors@epfl.ch>
2024-05-19 00:31:13 +0200

    test: create subcat

--- app/src/androidTest/java/com/github/se/assocify/screens/AddSubcategoryDialogTest.kt
+++ app/src/androidTest/java/com/github/se/assocify/screens/AddSubcategoryDialogTest.kt
@@ -7,24 +7,62 @@ import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.se.assocify.model.CurrentUser
+import com.github.se.assocify.model.database.AccountingCategoryAPI
+import com.github.se.assocify.model.database.AccountingSubCategoryAPI
+import com.github.se.assocify.model.entities.AccountingCategory
+import com.github.se.assocify.model.entities.AccountingSubCategory
+import com.github.se.assocify.ui.screens.treasury.accounting.AccountingViewModel
 import com.github.se.assocify.ui.screens.treasury.accounting.accountingComposables.AddSubcategoryDialog
 import com.kaspersky.components.composesupport.config.withComposeSupport
 import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import io.mockk.every
 import io.mockk.junit4.MockKRule
+import io.mockk.mockk
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class AddCategoryScreenTest : TestCase(kaspressoBuilder = Kaspresso.Builder.withComposeSupport()) {
+class AddSubcategoryDialogTest : TestCase(kaspressoBuilder = Kaspresso.Builder.withComposeSupport()) {
   @get:Rule val composeTestRule = createComposeRule()
   @get:Rule val mockkRule = MockKRule(this)
 
+  val categories = listOf(
+    AccountingCategory("1", "Pole"),
+    AccountingCategory("2", "Events"),
+    AccountingCategory("3", "Commission")
+  )
+
+  val subCategories = listOf(
+    AccountingSubCategory("1", "1", "Pole 1", 0, 2021),
+  )
+
+  val accountingCategoryAPI = mockk<AccountingCategoryAPI>()
+  val accountingSubCategoryAPI = mockk<AccountingSubCategoryAPI>()
+  lateinit var viewModel: AccountingViewModel
+
   @Before
-  fun display() {
-    composeTestRule.setContent { AddSubcategoryDialog(show = true) }
+  fun setup() {
+    CurrentUser.userUid = "userUid"
+    CurrentUser.associationUid = "associationUid"
+    every { accountingCategoryAPI.getCategories(any(), any(), any()) } answers {
+      secondArg<(List<AccountingCategory>) -> Unit>().invoke(categories)
+    }
+    every { accountingSubCategoryAPI.getSubCategories(any(), any(), any()) } answers {
+      secondArg<(List<AccountingSubCategory>) -> Unit>().invoke(subCategories)
+    }
+    every { accountingSubCategoryAPI.addSubCategory(any(), any(), any(), any()) } answers {
+      thirdArg<() -> Unit>().invoke()
+    }
+    viewModel = AccountingViewModel(
+      accountingCategoryAPI = accountingCategoryAPI,
+      accountingSubCategoryAPI = accountingSubCategoryAPI
+    )
+    composeTestRule.setContent { AddSubcategoryDialog(viewModel) }
+    viewModel.showNewSubcategoryDialog()
   }
 
   @Test
