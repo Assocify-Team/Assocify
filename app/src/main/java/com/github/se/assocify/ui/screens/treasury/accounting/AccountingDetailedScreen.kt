2ebdd175feb4a7f7ee88d41c7d806613337b137f
Lorenzo <lorenzo.padrini@epfl.ch>
2024-05-18 23:06:05 +0200

    fix: added padding on lower part of the screen

--- app/src/main/java/com/github/se/assocify/ui/screens/treasury/accounting/AccountingDetailedScreen.kt
+++ app/src/main/java/com/github/se/assocify/ui/screens/treasury/accounting/AccountingDetailedScreen.kt
@@ -5,8 +5,10 @@ import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -258,6 +260,7 @@ fun AccountingDetailedScreen(
               }
             }
           }
+          item { Spacer(modifier = Modifier.height(80.dp)) }
         }
       }
 }
