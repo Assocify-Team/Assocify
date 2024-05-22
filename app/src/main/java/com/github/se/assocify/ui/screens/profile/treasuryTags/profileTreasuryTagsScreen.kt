d13489512f6459880269509a3583b09fec92b550
Mai-LinhC <mai-linh.cordonnier@epfl.ch>
2024-05-22 15:32:24 +0200

    feat: basic screen with listItems

--- app/src/main/java/com/github/se/assocify/ui/screens/profile/treasuryTags/profileTreasuryTagsScreen.kt
+++ app/src/main/java/com/github/se/assocify/ui/screens/profile/treasuryTags/profileTreasuryTagsScreen.kt
@@ -2,23 +2,37 @@ package com.github.se.assocify.ui.screens.profile.treasuryTags
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.unit.dp
 import com.github.se.assocify.navigation.NavigationActions
 import com.github.se.assocify.ui.composables.BackButton
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ProfileTreasuryTagsScreen(navActions: NavigationActions) {
-  Scaffold(
+fun ProfileTreasuryTagsScreen(navActions: NavigationActions, treasuryTagsViewModel: ProfileTreasuryTagsViewModel) {
+    val state by treasuryTagsViewModel.uiState.collectAsState()
+
+    Scaffold(
       modifier = Modifier.testTag("TreasuryTags Screen"),
       topBar = {
         CenterAlignedTopAppBar(
@@ -29,13 +43,29 @@ fun ProfileTreasuryTagsScreen(navActions: NavigationActions) {
                   onClick = { navActions.back() },
                   modifier = Modifier.testTag("backButton"))
             })
-      }) {
-        Column(
-            modifier = Modifier.fillMaxSize().padding(it),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
+      },
+        contentWindowInsets = WindowInsets(20.dp, 10.dp, 20.dp, 20.dp)
+    ) {
+
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it),
         ) {
-          Text(modifier = Modifier.padding(it), text = "Treasury Tags Screen : not yet implemented")
+            state.treasuryTags.forEach { treasuryTag ->
+                item { ListItem(headlineContent = { Text(text = treasuryTag.name) },
+                    trailingContent = {
+
+                        Row {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Default.Edit, contentDescription = "Edit")
+                    }
+
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Default.Delete, contentDescription = "Delete")
+                    }
+                }}) }
+            }
         }
       }
 }
