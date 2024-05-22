4ba4a895bb420b791eeadd7c4830d1a0596d70bf
Mai-LinhC <mai-linh.cordonnier@epfl.ch>
2024-05-22 20:11:40 +0200

    feat: basic display for members

--- app/src/main/java/com/github/se/assocify/ui/screens/profile/members/ProfileMembersScreen.kt
+++ app/src/main/java/com/github/se/assocify/ui/screens/profile/members/ProfileMembersScreen.kt
@@ -1,11 +1,13 @@
 package com.github.se.assocify.ui.screens.profile.members
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,6 +16,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.unit.dp
 import com.github.se.assocify.navigation.NavigationActions
 import com.github.se.assocify.ui.composables.BackButton
 
@@ -35,12 +38,25 @@ fun ProfileMembersScreen(
                   onClick = { navActions.back() },
                   modifier = Modifier.testTag("backButton"))
             })
-      }) {
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.fillMaxSize().padding(it)) {
-              Text(text = "Members Screen : not yet implemented")
+      },
+      contentWindowInsets = WindowInsets(20.dp, 0.dp, 20.dp, 0.dp),
+  ) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize().padding(it)) {
+          Text(text = "New requests")
+          LazyColumn {
+            state.applicants.forEach {
+              item { ListItem(headlineContent = { Text(text = it.name) }) }
             }
-      }
+          }
+
+          Text(text = "Current members")
+          LazyColumn {
+            state.currMembers.forEach {
+              item { ListItem(headlineContent = { Text(text = it.name) }) }
+            }
+          }
+        }
+  }
 }
