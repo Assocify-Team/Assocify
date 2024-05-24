df719886e8f468e192cb2175180ff4138003373a
Sarah Badr <92681618+sarahbadr17@users.noreply.github.com>
2024-05-24 16:10:59 +0200

    style: remove import unused

--- app/src/main/java/com/github/se/assocify/ui/screens/treasury/receiptstab/receipt/ReceiptScreen.kt
+++ app/src/main/java/com/github/se/assocify/ui/screens/treasury/receiptstab/receipt/ReceiptScreen.kt
@@ -40,7 +40,6 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
