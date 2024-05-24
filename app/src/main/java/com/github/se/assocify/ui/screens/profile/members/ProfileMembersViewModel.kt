c0ffaa0a17d5d89324acdec8f2cb481a168e31ea
Mai-LinhC <mai-linh.cordonnier@epfl.ch>
2024-05-24 21:06:06 +0200

    fix: remove hardcoded list

--- app/src/main/java/com/github/se/assocify/ui/screens/profile/members/ProfileMembersViewModel.kt
+++ app/src/main/java/com/github/se/assocify/ui/screens/profile/members/ProfileMembersViewModel.kt
@@ -15,30 +15,11 @@ class ProfileMembersViewModel(navActions: NavigationActions, associationAPI: Ass
   private val _uiState = MutableStateFlow(ProfileMembersUIState())
   val uiState: StateFlow<ProfileMembersUIState> = _uiState
 
-  private val tempMemberList =
-      listOf(
-          User("1", "Sarah"),
-          User("2", "hjibcdsbqdihjvkbqkvjbqdsipvbjkdvbj"),
-          User("3", "Alice"),
-          User("4", "Bob"),
-          User("5", "Charlie"),
-          User("6", "David"),
-          User("7", "Eve"),
-          User("8", "Frank"),
-          User("9", "Grace"),
-          User("10", "Hank"),
-          User("11", "Ivy"),
-      )
-
   init {
     associationAPI.getApplicants(
         CurrentUser.associationUid!!,
         { applicants -> _uiState.value = _uiState.value.copy(applicants = applicants) },
         { Log.e("members", "Error loading applicants : ${it.message}") })
-    // to debug with a big list :
-    //    _uiState.value = _uiState.value.copy(applicants = tempMemberList.take(2))
-    // not yet done in API : getMembers of association
-    //    _uiState.value = _uiState.value.copy(currMembers = tempMemberList)
     associationAPI.getMembers(
         CurrentUser.associationUid!!,
         { members -> _uiState.value = _uiState.value.copy(currMembers = members) },
