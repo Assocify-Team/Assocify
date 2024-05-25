01ffbca963c7e4b382f9e64bfc5148ad0f36e9cc
SidonieBouthors <sidonie.bouthors@epfl.ch>
2024-05-25 17:05:12 +0200

    feat: pull down refresh composable

---
+++ app/src/main/java/com/github/se/assocify/ui/composables/PullDownRefreshBox.kt
@@ -0,0 +1,45 @@
+package com.github.se.assocify.ui.composables
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+/**
+ * A container to make a scrollable content pullable to refresh.
+ *
+ * NOTE:
+ * Pass the scaffold padding values to the box itself, NOT the content inside the box.
+ *
+ * @param refreshing Whether the content is currently refreshing.
+ * @param onRefresh The callback to call when the user pulls down to refresh.
+ * @param paddingValues The padding values to apply to the conatiner.
+ */
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun PullDownRefreshBox(
+    refreshing: Boolean,
+    onRefresh: () -> Unit,
+    paddingValues: PaddingValues,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit = {},
+) {
+    val pullRefreshState = rememberPullRefreshState(refreshing, onRefresh)
+
+    Box(
+        modifier = modifier
+            .padding(paddingValues)
+            .fillMaxSize()
+            .pullRefresh(pullRefreshState)
+    ) {
+        content()
+        PullRefreshIndicator(refreshing, pullRefreshState, Modifier.align(Alignment.TopCenter))
+    }
+}
