3f54153c1ca5abaed1f11a13fb6f819b14e4309e
SidonieBouthors <sidonie.bouthors@epfl.ch>
2024-05-26 15:45:52 +0200

    style: ktfmt

--- app/src/main/java/com/github/se/assocify/ui/util/SnackbarSystem.kt
+++ app/src/main/java/com/github/se/assocify/ui/util/SnackbarSystem.kt
@@ -6,20 +6,20 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-/**
- * A system to show snackbars in the app.
- */
+/** A system to show snackbars in the app. */
 class SnackbarSystem(private val snackbarHostState: SnackbarHostState) {
   /**
    * Shows a snackbar with the given message.
    *
-   * Note : Persistent snackbars with no action will have a cross to dismiss them even if dismiss is false
+   * Note : Persistent snackbars with no action will have a cross to dismiss them even if dismiss is
+   * false
    *
    * @param message the message to show
    * @param actionLabel the label for the action button
    * @param actionHandler the handler for the action button
    * @param dismiss whether the snackbar should have a cross to dismiss it
-   * @param persistent whether the snackbar should remain until explicitely dismissed (otherwise, times out on its own)
+   * @param persistent whether the snackbar should remain until explicitely dismissed (otherwise,
+   *   times out on its own)
    */
   fun showSnackbar(
       message: String,
@@ -30,7 +30,9 @@ class SnackbarSystem(private val snackbarHostState: SnackbarHostState) {
   ) {
 
     CoroutineScope(Dispatchers.Main).launch {
-      val result = snackbarHostState.showSnackbar(message, actionLabel, dismiss || (persistent && actionLabel == null))
+      val result =
+          snackbarHostState.showSnackbar(
+              message, actionLabel, dismiss || (persistent && actionLabel == null))
       if (result == SnackbarResult.ActionPerformed) {
         actionHandler()
       }
