4a64a382c02f282777881c0334a3c92dd9332c24
Mai-LinhC <mai-linh.cordonnier@epfl.ch>
2024-05-29 21:38:08 +0200

    feat: comments and how to add event

--- app/src/main/java/com/github/se/assocify/ui/screens/profile/events/profileEventsViewModel.kt
+++ app/src/main/java/com/github/se/assocify/ui/screens/profile/events/profileEventsViewModel.kt
@@ -5,10 +5,12 @@ import androidx.lifecycle.ViewModel
 import com.github.se.assocify.model.database.EventAPI
 import com.github.se.assocify.model.entities.Event
 import com.github.se.assocify.navigation.NavigationActions
+import java.time.OffsetDateTime
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
-class ProfileEventsViewModel(private val eventAPI: EventAPI, navActions: NavigationActions) : ViewModel() {
+class ProfileEventsViewModel(private val eventAPI: EventAPI, navActions: NavigationActions) :
+    ViewModel() {
   private val _uiState = MutableStateFlow(ProfileEventsUIState())
   val uiState: StateFlow<ProfileEventsUIState> = _uiState
 
@@ -18,41 +20,89 @@ class ProfileEventsViewModel(private val eventAPI: EventAPI, navActions: Navigat
         { Log.e("events", "Error loading events") })
   }
 
-    fun modifyEvent(event: Event) {
-        _uiState.value = _uiState.value.copy(modifyingEvent = event)
-    }
+  /**
+   * Opens the dialogue for modifying an event and sets the event to modify
+   *
+   * @param event the event to modify
+   */
+  fun modifyEvent(event: Event) {
+    _uiState.value = _uiState.value.copy(modifyingEvent = event)
+    _uiState.value = _uiState.value.copy(openDialogue = true)
+  }
 
-    fun clearModifyingEvent() {
-        _uiState.value = _uiState.value.copy(modifyingEvent = null)
-    }
+  /** Closes the dialogue for modifying an event without saving the changes */
+  fun clearModifyingEvent() {
+    _uiState.value = _uiState.value.copy(modifyingEvent = null)
+    _uiState.value = _uiState.value.copy(openDialogue = false)
+  }
 
-    fun deleteCurrentEvent() {
-        eventAPI.deleteEvent(_uiState.value.modifyingEvent!!.uid,
-            { eventAPI.getEvents(
-                { eventList -> _uiState.value = _uiState.value.copy(events = eventList) },
-                { Log.e("events", "Error loading events") }) },
-            { Log.e("events", "Error deleting event") })
-    }
+  /** Deletes the event that is currently being modified */
+  fun deleteCurrentEvent() {
+    eventAPI.deleteEvent(
+        _uiState.value.modifyingEvent!!.uid,
+        {
+          eventAPI.getEvents(
+              { eventList -> _uiState.value = _uiState.value.copy(events = eventList) },
+              { Log.e("events", "Error loading events") })
+        },
+        { Log.e("events", "Error deleting event") })
+  }
 
-    fun updateCurrentEvent() {
-        eventAPI.updateEvent(_uiState.value.modifyingEvent!!,
-            { eventAPI.getEvents(
-                { eventList -> _uiState.value = _uiState.value.copy(events = eventList) },
-                { Log.e("events", "Error loading events") }) },
-            { Log.e("events", "Error updating event") })
-    }
+  /** Updates the event that is currently being modified */
+  fun updateCurrentEvent() {
+    eventAPI.updateEvent(
+        _uiState.value.modifyingEvent!!,
+        {
+          eventAPI.getEvents(
+              { eventList -> _uiState.value = _uiState.value.copy(events = eventList) },
+              { Log.e("events", "Error loading events") })
+        },
+        { Log.e("events", "Error updating event") })
+  }
 
-    fun addEvent(event: Event) {
-        eventAPI.addEvent(event,
-            { eventAPI.getEvents(
-                { eventList -> _uiState.value = _uiState.value.copy(events = eventList) },
-                { Log.e("events", "Error loading events") }) },
-            { Log.e("events", "Error adding event") })
-    }
+  /** Opens the dialogue for adding an event */
+  fun openAddEvent() {
+    _uiState.value = _uiState.value.copy(openDialogue = true)
+  }
+
+  /** Adds the event that is being created */
+  fun confirmAddEvent() {
+    val event =
+        Event(
+            name = _uiState.value.newName,
+            description = _uiState.value.newDescription,
+            startDate = _uiState.value.newStartDate,
+            endDate = _uiState.value.newEndDate,
+            guestsOrArtists = _uiState.value.newGuestsOrArtists,
+            location = _uiState.value.newLocation)
+    eventAPI.addEvent(
+        event,
+        {
+          eventAPI.getEvents(
+              { eventList -> _uiState.value = _uiState.value.copy(events = eventList) },
+              { Log.e("events", "Error loading events") })
+        },
+        { Log.e("events", "Error adding event") })
+  }
 }
 
 data class ProfileEventsUIState(
+    // The list of current events
     val events: List<Event> = emptyList(),
+    // The event that is currently being modified
     val modifyingEvent: Event? = null,
-
+    // Whether the dialogue for adding or modifying an event is open
+    val openDialogue: Boolean = false,
+    // The name for the new event
+    val newName: String = "",
+    // The description for the new event
+    val newDescription: String = "",
+    // The start date for the new event
+    val newStartDate: OffsetDateTime = OffsetDateTime.now(),
+    // The end date for the new event
+    val newEndDate: OffsetDateTime = OffsetDateTime.now(),
+    // The guests or artists for the new event
+    val newGuestsOrArtists: String = "",
+    // The location for the new event
+    val newLocation: String = ""
 )
