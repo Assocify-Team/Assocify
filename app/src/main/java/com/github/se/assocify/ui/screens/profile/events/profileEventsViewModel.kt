c0777e171b5c746babf0da9f815668ecc7c33412
Mai-LinhC <mai-linh.cordonnier@epfl.ch>
2024-05-29 21:02:04 +0200

    feat: basic functions in vm

--- app/src/main/java/com/github/se/assocify/ui/screens/profile/events/profileEventsViewModel.kt
+++ app/src/main/java/com/github/se/assocify/ui/screens/profile/events/profileEventsViewModel.kt
@@ -8,7 +8,7 @@ import com.github.se.assocify.navigation.NavigationActions
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
-class ProfileEventsViewModel(eventAPI: EventAPI, navActions: NavigationActions) : ViewModel() {
+class ProfileEventsViewModel(private val eventAPI: EventAPI, navActions: NavigationActions) : ViewModel() {
   private val _uiState = MutableStateFlow(ProfileEventsUIState())
   val uiState: StateFlow<ProfileEventsUIState> = _uiState
 
@@ -17,6 +17,42 @@ class ProfileEventsViewModel(eventAPI: EventAPI, navActions: NavigationActions) 
         { eventList -> _uiState.value = _uiState.value.copy(events = eventList) },
         { Log.e("events", "Error loading events") })
   }
+
+    fun modifyEvent(event: Event) {
+        _uiState.value = _uiState.value.copy(modifyingEvent = event)
+    }
+
+    fun clearModifyingEvent() {
+        _uiState.value = _uiState.value.copy(modifyingEvent = null)
+    }
+
+    fun deleteCurrentEvent() {
+        eventAPI.deleteEvent(_uiState.value.modifyingEvent!!.uid,
+            { eventAPI.getEvents(
+                { eventList -> _uiState.value = _uiState.value.copy(events = eventList) },
+                { Log.e("events", "Error loading events") }) },
+            { Log.e("events", "Error deleting event") })
+    }
+
+    fun updateCurrentEvent() {
+        eventAPI.updateEvent(_uiState.value.modifyingEvent!!,
+            { eventAPI.getEvents(
+                { eventList -> _uiState.value = _uiState.value.copy(events = eventList) },
+                { Log.e("events", "Error loading events") }) },
+            { Log.e("events", "Error updating event") })
+    }
+
+    fun addEvent(event: Event) {
+        eventAPI.addEvent(event,
+            { eventAPI.getEvents(
+                { eventList -> _uiState.value = _uiState.value.copy(events = eventList) },
+                { Log.e("events", "Error loading events") }) },
+            { Log.e("events", "Error adding event") })
+    }
 }
 
-data class ProfileEventsUIState(val events: List<Event> = emptyList())
+data class ProfileEventsUIState(
+    val events: List<Event> = emptyList(),
+    val modifyingEvent: Event? = null,
+
+)
