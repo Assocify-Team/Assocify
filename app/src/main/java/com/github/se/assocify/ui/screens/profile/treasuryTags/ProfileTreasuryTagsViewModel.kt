85aa100cded58578de74dbdea98bf86125a1a89e
Lorenzo <lorenzo.padrini@epfl.ch>
2024-06-01 16:40:00 +0200

    feat: added the viewModel

---
+++ app/src/main/java/com/github/se/assocify/ui/screens/profile/treasuryTags/ProfileTreasuryTagsViewModel.kt
@@ -0,0 +1,81 @@
+package com.github.se.assocify.ui.screens.profile.treasuryTags
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import com.github.se.assocify.model.CurrentUser
+import com.github.se.assocify.model.database.AccountingCategoryAPI
+import com.github.se.assocify.model.entities.AccountingCategory
+import com.github.se.assocify.navigation.NavigationActions
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class ProfileTreasuryTagsViewModel(
+    private val accountingCategoryAPI: AccountingCategoryAPI,
+    navActions: NavigationActions
+) : ViewModel() {
+  private val _uiState = MutableStateFlow(ProfileTreasuryTagsUIState())
+  val uiState: StateFlow<ProfileTreasuryTagsUIState> = _uiState
+
+  init {
+    accountingCategoryAPI.getCategories(
+        CurrentUser.associationUid!!,
+        { categoryList -> _uiState.value = _uiState.value.copy(treasuryTags = categoryList) },
+        { Log.e("treasurytags", "Error loading tags") })
+  }
+
+  fun modifying(modifying: Boolean, editedTag: AccountingCategory) {
+    _uiState.value =
+        _uiState.value.copy(
+            modify = modifying, displayedName = "Editing tag", editedTag = editedTag)
+  }
+
+  fun creating(modifying: Boolean) {
+    _uiState.value = _uiState.value.copy(creating = modifying, displayedName = "Creating tag")
+  }
+
+  fun deleteTag(tag: AccountingCategory) {
+    accountingCategoryAPI.deleteCategory(
+        tag,
+        {
+          _uiState.value =
+              _uiState.value.copy(treasuryTags = _uiState.value.treasuryTags.filter { tag != it })
+        },
+        {})
+  }
+
+  fun addTag(newTag: AccountingCategory) {
+    accountingCategoryAPI.addCategory(
+        associationUID = _uiState.value.assocId,
+        newTag,
+        {
+          _uiState.value = _uiState.value.copy(treasuryTags = _uiState.value.treasuryTags + newTag)
+        },
+        {})
+  }
+
+  fun modifyTag(tag: AccountingCategory) {
+    accountingCategoryAPI.updateCategory(
+        _uiState.value.assocId,
+        tag,
+        {
+          _uiState.value =
+              _uiState.value.copy(
+                  treasuryTags = _uiState.value.treasuryTags.filter { it.uid != tag.uid } + tag)
+        },
+        {})
+  }
+
+  fun cancelPopUp() {
+    _uiState.value =
+        _uiState.value.copy(modify = false, creating = false, editedTag = null, displayedName = "")
+  }
+}
+
+data class ProfileTreasuryTagsUIState(
+    val assocId: String = CurrentUser.associationUid!!,
+    val treasuryTags: List<AccountingCategory> = emptyList(),
+    val modify: Boolean = false,
+    val creating: Boolean = false,
+    val editedTag: AccountingCategory? = null,
+    val displayedName: String = ""
+)
