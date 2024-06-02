d5e53b95321673790857ab5d8c6fcd175dc9e556
rayan <rayan.boucheny@epfl.ch>
2024-06-02 20:48:46 +0200

    feat: changed merged conflict stuff

--- app/src/main/java/com/github/se/assocify/ui/screens/event/maptab/EventMapScreen.kt
+++ app/src/main/java/com/github/se/assocify/ui/screens/event/maptab/EventMapScreen.kt
@@ -36,10 +36,6 @@ import org.osmdroid.views.overlay.TilesOverlay
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
-// Initial position of the map (EPFL Agora)
-private val INITIAL_POSITION = GeoPoint(46.518726, 6.566613)
-// Initial zoom of the map (Zoom made to be focused on the EPFL campus)
-private const val INITIAL_ZOOM = 17.0
 // The location overlay to display the user's location
 private var myLocationOverlay: MyLocationNewOverlay? = null
 // Permission code for accessing GPS location
@@ -194,23 +190,4 @@ fun requestLocationPermission(context: Context): Boolean {
     return false
   }
   return true
-}
-
-/**
- * The custom tile source from the EPFL plan API.
- *
- * @param floorId the floor id of the map to display
- */
-class CampusTileSource(private val floorId: Int) :
-    OnlineTileSourceBase("EPFLCampusTileSource", 0, 18, 256, ".png", arrayOf()) {
-  override fun getTileURLString(pMapTileIndex: Long): String {
-    // Select at random the map server to use
-    val epflCampusServerCount = 3
-    // EPFL plan API has 3 servers, tilesX correspond to the server number
-    return "https://plan-epfl-tiles${Random.nextInt(epflCampusServerCount)}.epfl.ch/1.0.0/batiments/default/20160712/$floorId/3857/${
-      MapTileIndex.getZoom(
-        pMapTileIndex
-      )
-    }/${MapTileIndex.getY(pMapTileIndex)}/${MapTileIndex.getX(pMapTileIndex)}.png"
-  }
 }
