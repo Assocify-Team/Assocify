d85a14123da73938c2e0a0593d903e1ed4b48801
Mai-LinhC <mai-linh.cordonnier@epfl.ch>
2024-06-02 04:14:45 +0200

    feat: basic functions

--- app/src/main/java/com/github/se/assocify/ui/screens/profile/members/ProfileMembersViewModel.kt
+++ app/src/main/java/com/github/se/assocify/ui/screens/profile/members/ProfileMembersViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import com.github.se.assocify.model.CurrentUser
 import com.github.se.assocify.model.database.AssociationAPI
 import com.github.se.assocify.model.entities.AssociationMember
+import com.github.se.assocify.model.entities.RoleType
 import com.github.se.assocify.navigation.NavigationActions
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -20,8 +21,42 @@ class ProfileMembersViewModel(navActions: NavigationActions, associationAPI: Ass
         { members -> _uiState.value = _uiState.value.copy(currMembers = members) },
         { Log.e("members", "Error loading members : ${it.message}") })
   }
+
+  fun onEditMember(member: AssociationMember) {
+    _uiState.value = _uiState.value.copy(updatingMember = member, showEditMemberDialog = true)
+  }
+
+  fun onDeleteMember(member: AssociationMember) {
+    _uiState.value = _uiState.value.copy(updatingMember = member, showDeleteMemberDialog = true)
+  }
+
+  fun onEditMemberDialogDismiss() {
+    _uiState.value = _uiState.value.copy(showEditMemberDialog = false, updatingMember = null)
+  }
+
+  fun onDeleteMemberDialogDismiss() {
+    _uiState.value = _uiState.value.copy(showDeleteMemberDialog = false, updatingMember = null)
+  }
+
+  fun confirmDeleteMember() {
+    _uiState.value = _uiState.value.copy(showDeleteMemberDialog = false, updatingMember = null)
+  }
+
+  fun updateRole(newRole: RoleType) {
+    _uiState.value = _uiState.value.copy(newRole = newRole)
+  }
+
+  fun confirmEditMember() {
+    // update the member role in DB
+    _uiState.value =
+        _uiState.value.copy(showEditMemberDialog = false, updatingMember = null, newRole = null)
+  }
 }
 
 data class ProfileMembersUIState(
     val currMembers: List<AssociationMember> = emptyList(),
+    val updatingMember: AssociationMember? = null,
+    val newRole: RoleType? = null,
+    val showEditMemberDialog: Boolean = false,
+    val showDeleteMemberDialog: Boolean = false,
 )
